4b35b0a87ba70815f9647be8a43c91e496d2b5ea
Eduard Zingerman <eddyz87@gmail.com>
2023-02-14 00:49:33 +0200

    [DebugInfo][BPF] Add 'annotations' field for DIBasicType & DISubroutineType
    Extend DIBasicType and DISubroutineType with additional field
    'annotations', e.g. as below:
    
      !5 = !DIBasicType(name: "int", size: 32, encoding: DW_ATE_signed, annotations: !6)
      !6 = !{!7}
      !7 = !{!"btf:type_tag", !"tag1"}
    
    The field would be used by BPF backend to generate DWARF attributes
    corresponding to "btf_type_tag" type attributes, e.g.:
    
      0x00000029:   DW_TAG_base_type
                      DW_AT_name	("int")
                      DW_AT_encoding	(DW_ATE_signed)
                      DW_AT_byte_size	(0x04)
    
      0x0000002d:     DW_TAG_LLVM_annotation
                        DW_AT_name	("btf:type_tag")
                        DW_AT_const_value	("tag1")
    
    Such DWARF entries would be used to generate BTF definitions by tools
    like pahole [1].
    
    Note: similar fields with similar purposes are already present in
    DIDerivedType and DICompositeType.
    
    Currently "btf_type_tag" attributes are represented in debug
    information as 'annotations' fields in DIDerivedType with
    DW_TAG_pointer_type tag. The annotation on a pointer corresponds to
    pointee having the attributes in the final BTF.
    
    The discussion at [2] came to conclusion, that such annotations should
    apply to the annotated type itself. Hence the necessity to extend
    DIBasicType & DISubroutineType types with 'annotations' field to
    represent cases like below:
    
      int __attribute__((btf_type_tag("foo"))) bar;
    
    [1] https://github.com/acmel/dwarves
    [2] https://lore.kernel.org/bpf/87r0w9jjoq.fsf@oracle.com/
    
    This was previously tracked as differential revision:
    https://reviews.llvm.org/D143966

--- llvm/lib/IR/AsmWriter.cpp
+++ llvm/lib/IR/AsmWriter.cpp
@@ -2119,9 +2119,9 @@ static void writeDIEnumerator(raw_ostream &Out, const DIEnumerator *N,
 }
 
 static void writeDIBasicType(raw_ostream &Out, const DIBasicType *N,
-                             AsmWriterContext &) {
+                             AsmWriterContext &WriterCtx) {
   Out << "!DIBasicType(";
-  MDFieldPrinter Printer(Out);
+  MDFieldPrinter Printer(Out, WriterCtx);
   if (N->getTag() != dwarf::DW_TAG_base_type)
     Printer.printTag(N);
   Printer.printString("name", N->getName());
@@ -2130,6 +2130,7 @@ static void writeDIBasicType(raw_ostream &Out, const DIBasicType *N,
   Printer.printDwarfEnum("encoding", N->getEncoding(),
                          dwarf::AttributeEncodingString);
   Printer.printDIFlags("flags", N->getFlags());
+  Printer.printMetadata("annotations", N->getRawAnnotations());
   Out << ")";
 }
 
@@ -2225,6 +2226,7 @@ static void writeDISubroutineType(raw_ostream &Out, const DISubroutineType *N,
   Printer.printDwarfEnum("cc", N->getCC(), dwarf::ConventionString);
   Printer.printMetadata("types", N->getRawTypeArray(),
                         /* ShouldSkipNull */ false);
+  Printer.printMetadata("annotations", N->getRawAnnotations());
   Out << ")";
 }
 
