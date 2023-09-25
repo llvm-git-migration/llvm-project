f902d719030445ee64c7e9d50c21cc65680857a4
Mel Chen <mel.chen@sifive.com>
2023-09-25 00:54:22 -0700

    [LoopVectorize] Vectorize select-cmp reduction pattern for increasing integer induction variable
    Consider the following loop:
    
      int rdx = init;
      for (int i = 0; i < n; ++i)
        rdx = (a[i] > b[i]) ? i : rdx;
    
    We can vectorize this loop if `i` is an increasing induction variable.
    The final reduced value will be the maximum of `i` that the condition
    `a[i] > b[i]` is satisfied, or the start value `init`.
    
    This patch added new RecurKind enums - IFindLastIV and FFindLastIV.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -20521,6 +20521,8 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::IAnyOf:
     case RecurKind::FAnyOf:
+    case RecurKind::IFindLastIV:
+    case RecurKind::FFindLastIV:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for repeated scalar.");
     }
@@ -20618,6 +20620,8 @@ private:
     case RecurKind::FMulAdd:
     case RecurKind::IAnyOf:
     case RecurKind::FAnyOf:
+    case RecurKind::IFindLastIV:
+    case RecurKind::FFindLastIV:
     case RecurKind::None:
       llvm_unreachable("Unexpected reduction kind for reused scalars.");
     }
