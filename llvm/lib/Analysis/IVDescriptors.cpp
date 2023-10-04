7d2a39583799f29e00fff5ee42bf79683355a479
Mel Chen <mel.chen@sifive.com>
2023-10-04 15:21:19 +0800

    Modified lambda to capture all external variables by reference
    Co-authored-by: Alexey Bataev <5361294+alexey-bataev@users.noreply.github.com>

--- llvm/lib/Analysis/IVDescriptors.cpp
+++ llvm/lib/Analysis/IVDescriptors.cpp
@@ -711,7 +711,7 @@ RecurrenceDescriptor::isFindLastIVPattern(Loop *Loop, PHINode *OrigPhi,
   else
     return InstDesc(false, I);
 
-  auto IsIncreasingLoopInduction = [&SE, &Loop](Value *V) {
+  auto IsIncreasingLoopInduction = [&](Value *V) {
     if (!SE)
       return false;
 
