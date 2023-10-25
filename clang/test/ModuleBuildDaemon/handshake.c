9a878a711ec44193b1d0d4eeb774a7ca4eeec070
cpsughrue <cpsughrue@gmail.com>
2023-10-25 13:56:27 -0400

    Address feedback
    - In readFromSocket change assign to append
    - Change token used to find end of YAML document
    - Remove condition from writeToSocket that will never be met
    - Change exit to _Exit in signal handle
    - Remove unessary use of remark and update test
    - Convert warning to error for when socket address is too long to fit in unix_path.sun_addr
    - Improve diagnositc options for module build daemon

--- clang/test/ModuleBuildDaemon/handshake.c
+++ clang/test/ModuleBuildDaemon/handshake.c
@@ -1,6 +1,3 @@
-// COM: Check that clang invocation can spawn and handshake with module build daemon
-// COM: Also check that clang invocation can handshake with existing module build daemon
-
 // REQUIRES: !system-windows
 
 //  RUN: if pgrep -f "cc1modbuildd mbd-handshake"; then pkill -f "cc1modbuildd mbd-handshake"; fi
@@ -15,21 +12,12 @@ int main() {return 0;}
 // RUN: %clang -fmodule-build-daemon=mbd-handshake -Rmodule-build-daemon %t/main.c &> %t/output-existing
 // RUN: cat %t/output-existing | FileCheck %s --check-prefix=CHECK-EXIST
 
-// CHECK: remark: Trying to spawn module build daemon [-Rmodule-build-daemon]
+// COM: Check that clang invocation can spawn and handshake with module build daemon
 // CHECK: remark: Successfully spawned module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Trying to connect to recently spawned module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Succesfully connected to recently spawned module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Trying to send HandshakeMsg to module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Successfully sent HandshakeMsg to module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Waiting to receive module build daemon response [-Rmodule-build-daemon]
-// CHECK: remark: Successfully received HandshakeMsg from module build daemon [-Rmodule-build-daemon]
-// CHECK: remark: Completed successfull handshake with module build daemon [-Rmodule-build-daemon]
+// CHECK: remark: Successfully connected to module build daemon at mbd-handshake/mbd.sock [-Rmodule-build-daemon]
+// CHECK: remark: Successfully completed handshake with module build daemon [-Rmodule-build-daemon]
 
-// CHECK-EXIST: remark: Module build daemon already exists [-Rmodule-build-daemon]
-// CHECK-EXIST: remark: Trying to send HandshakeMsg to module build daemon [-Rmodule-build-daemon]
-// CHECK-EXIST: remark: Successfully sent HandshakeMsg to module build daemon [-Rmodule-build-daemon]
-// CHECK-EXIST: remark: Waiting to receive module build daemon response [-Rmodule-build-daemon]
-// CHECK-EXIST: remark: Successfully received HandshakeMsg from module build daemon [-Rmodule-build-daemon]
-// CHECK-EXIST: remark: Completed successfull handshake with module build daemon [-Rmodule-build-daemon]
+// COM: Check that clang invocation can handshake with existing module build daemon
+// CHECK-EXIST: remark: Successfully completed handshake with module build daemon [-Rmodule-build-daemon]
 
 // RUN: if pgrep -f "cc1modbuildd mbd-handshake"; then pkill -f "cc1modbuildd mbd-handshake"; fi
