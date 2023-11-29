df5f1489e9e44499e060645b7917d53a07e0220b
Rajveer <rajveer.developer@icloud.com>
2023-11-29 15:55:25 +0530

    [libc++] Diagnoses insufficiently aligned pointers for std::assume_aligned during constant evaluation
    Resolves Issue #64078
    
    This is a libc++ enhancement when violating alignment assumption for __builtin_assume_aligned.

---
+++ libcxx/test/libcxx/utilities/memory/ptr.align/assume_aligned.const_eval.verify.cpp
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <memory>
+#include <cstddef>
+
+template <size_t Size>
+constexpr bool test() {
+  char data[1];
+
+  auto data1 = std::assume_aligned<Size>(data);
+  delete data1;
+
+  return true;
+}
+
+static_assert(
+    test<2>(),
+    "asserted 2 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 2 bytes}}
+
+static_assert(
+    test<4>(),
+    "asserted 4 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 4 bytes}}
+
+static_assert(
+    test<8>(),
+    "asserted 8 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 8 bytes}}
+
+static_assert(
+    test<16>(),
+    "asserted 16 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 16 bytes}}
+
+static_assert(
+    test<32>(),
+    "asserted 32 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 32 bytes}}
+
+static_assert(
+    test<64>(),
+    "asserted 64 bytes."); // expected-error {{static assertion expression is not an integral constant expression}}
+// expected-note@* {{alignment of the base pointee object (1 byte) is less than the asserted 64 bytes}}
