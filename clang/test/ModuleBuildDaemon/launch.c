2fdd061f1c5f21d03c943197163836253fbab818
cpsughrue <cpsughrue@gmail.com>
2023-12-07 00:38:43 -0500

    Now that ExecuteNoWait can detach the spawned process update forkDaemon and spawnModuleBuildDaemon

--- clang/test/ModuleBuildDaemon/launch.c
+++ clang/test/ModuleBuildDaemon/launch.c
@@ -1,13 +1,11 @@
 // Check that the module build daemon can create a unix socket
 
 // REQUIRES: !system-windows
-
-// RUN: if pgrep -f "cc1modbuildd mbd-launch"; then pkill -f "cc1modbuildd mbd-launch"; fi
 // RUN: rm -rf mbd-launch %t
 
-// RUN: %clang -cc1modbuildd mbd-launch -v
+// The module build daemon relies on llvm::sys::ExecuteNoWait to be detached from the 
+// terminal so when using -cc1modbuildd the command needs to be killed manually
+// RUN: timeout --preserve-status --signal=SIGTERM 2 %clang -cc1modbuildd mbd-launch -v
 // RUN: cat mbd-launch/mbd.out | FileCheck %s
 
 // CHECK: mbd created and binded to socket at: mbd-launch/mbd.sock
-
-// RUN: if pgrep -f "cc1modbuildd mbd-launch"; then pkill -f "cc1modbuildd mbd-launch"; fi
