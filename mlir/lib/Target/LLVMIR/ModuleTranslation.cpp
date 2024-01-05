1da9dd87fee76343eec078f32e60bd1780efda66
Andrew Gozillon <Andrew.Gozillon@amd.com>
2024-01-05 10:10:33 -0600

    remove debug artifact I added

--- mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
+++ mlir/lib/Target/LLVMIR/ModuleTranslation.cpp
@@ -1613,8 +1613,6 @@ mlir::translateModuleToLLVMIR(Operation *module, llvm::LLVMContext &llvmContext,
   if (failed(translator.convertFunctions()))
     return nullptr;
 
-  // translator.llvmModule->dump();
-
   if (llvm::verifyModule(*translator.llvmModule, &llvm::errs()))
     return nullptr;
 
