ca753eef960553920e57c1f890fa9e42fe335535
Paul Kirth <pk1574@gmail.com>
2024-01-08 23:36:27 +0000

    Update error string to mach checks, and fix tests

--- llvm/test/MC/RISCV/rv32-tlsdesc-valid.s
+++ llvm/test/MC/RISCV/rv32-tlsdesc-valid.s
@@ -1,16 +1,18 @@
-# RUN: llvm-mc -filetype=obj -triple riscv32 < %s | llvm-objdump -d -M no-aliases - | FileCheck %s
+# RUN: llvm-mc -filetype=obj -triple riscv32 < %s | llvm-objdump -d -M no-aliases - | FileCheck %s --check-prefix=INST
 
 start:                                  # @start
 # %bb.0:                                # %entry
 .Ltlsdesc_hi0:
+	auipc a0, %tlsdesc_hi(a-4)
+	# INST: auipc a0, 0x0
 	auipc	a0, %tlsdesc_hi(unspecified)
-	#CHECK: auipc a0, 0x0
+	# INST: auipc a0, 0x0
 	lw	a1, %tlsdesc_load_lo(.Ltlsdesc_hi0)(a0)
-	#CHECK: lw a1, 0x0(a0)
+	# INST: lw a1, 0x0(a0)
 	addi	a0, a0, %tlsdesc_add_lo(.Ltlsdesc_hi0)
-	#CHECK: addi a0, a0, 0x0
+	# INST: addi a0, a0, 0x0
 	jalr	t0, 0(a1), %tlsdesc_call(.Ltlsdesc_hi0)
-	#CHECK: jalr t0, 0x0(a1)
+	# INST: jalr t0, 0x0(a1)
 	add	a0, a0, tp
-	#CHECK: add a0, a0, tp
+	# INST: add a0, a0, tp
 	ret
--- llvm/test/MC/RISCV/tlsdesc-invalid.s
+++ llvm/test/MC/RISCV/tlsdesc-invalid.s
@@ -1,8 +1,9 @@
 # RUN: not llvm-mc %s -triple=riscv32 2>&1 | FileCheck %s
 # RUN: not llvm-mc %s -triple=riscv64 2>&1 | FileCheck %s
 
-auipc x1, %tlsdesc_call(foo) # CHECK: :[[@LINE]]:11: error: operand must be a symbol with a %pcrel_hi/%got_pcrel_hi/%tls_ie_pcrel_hi/%tls_gd_pcrel_hi modifier or an integer in the range
-auipc x1, %tlsdesc_call(1234) # CHECK: :[[@LINE]]:11: error: operand must be a symbol with a %pcrel_hi/%got_pcrel_hi/%tls_ie_pcrel_hi/%tls_gd_pcrel_hi modifier or an integer in the range
+auipc x1, %tlsdesc_call(foo) # CHECK: :[[@LINE]]:11: error: operand must be a symbol with a %pcrel_hi/%got_pcrel_hi/%tls_ie_pcrel_hi/%tls_gd_pcrel_hi/%tlsdesc_hi modifier or an integer in the range
+auipc x1, %tlsdesc_call(1234) # CHECK: :[[@LINE]]:11: error: operand must be a symbol with a %pcrel_hi/%got_pcrel_hi/%tls_ie_pcrel_hi/%tls_gd_pcrel_hi/%tlsdesc_hi modifier or an integer in the range
+auipc a0, %tlsdesc_hi(a+b) # CHECK: :[[@LINE]]:11: error: operand must be a symbol with a %pcrel_hi/%got_pcrel_hi/%tls_ie_pcrel_hi/%tls_gd_pcrel_hi/%tlsdesc_hi modifier or an integer in the range
 
 lw   a0, t0, %tlsdesc_load_lo(a_symbol) # CHECK: :[[@LINE]]:14: error: invalid operand for instruction
 lw   a0, t0, %tlsdesc_load_lo(a_symbol)(a4) # CHECK: :[[@LINE]]:14: error: invalid operand for instruction
