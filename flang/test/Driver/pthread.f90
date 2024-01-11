ae2e23569616f4cb13b4eaec902803650f0fc1b4
Tarun Prabhu <tarun.prabhu@gmail.com>
2024-01-11 12:40:30 -0700

    Add comments to the test explaining why it is difficult to write a failing test for the -pthread option.

--- flang/test/Driver/pthread.f90
+++ flang/test/Driver/pthread.f90
@@ -1,3 +1,15 @@
+! In release 2.34, glibc removed libpthread as a separate library. All the
+! pthread_* functions were subsumed into libc, so linking that is sufficient.
+! However, when linking against older glibc builds, the explicit link of
+! -pthread will be required. More details are here:
+!
+! https://developers.redhat.com/articles/2021/12/17/why-glibc-234-removed-libpthread#the_developer_view
+!
+! This makes it difficult to write a test that requires the -pthread flag in
+! order to pass. Checking for the presence of -lpthread in the linker flags is
+! not reliable since the linker could just skip the flag altogether if it is
+! linking against a new libc implementation.
+
 ! RUN: %flang -### -pthread /dev/null -o /dev/null 2>&1 | FileCheck %s
 ! RUN: %flang -### -Xflang -pthread /dev/null -o /dev/null 2>&1 | FileCheck %s
 
