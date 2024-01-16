74cfe79abc8fac70a8f551574d25437a18d70b43
justinfargnoli <justinfargnoli@gmail.com>
2024-01-16 15:58:28 -0800

    Declare most other dialect interface implementations

--- mlir/lib/Dialect/Linalg/IR/LinalgDialect.cpp
+++ mlir/lib/Dialect/Linalg/IR/LinalgDialect.cpp
@@ -21,6 +21,8 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/DialectImplementation.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
+#include "mlir/Interfaces/SubsetOpInterface.h"
+#include "mlir/Interfaces/ValueBoundsOpInterface.h"
 #include "mlir/Parser/Parser.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/InliningUtils.h"
@@ -118,6 +120,9 @@ void mlir::linalg::LinalgDialect::initialize() {
       >(namedStructuredOpRegionBuilders);
 
   addInterfaces<LinalgInlinerInterface>();
+  declarePromisedInterface<CopyOp, SubsetOpInterface>();
+  declarePromisedInterface<CopyOp, SubsetInsertionOpInterface>();
+  declarePromisedInterface<IndexOp, ValueBoundsOpInterface>();
 }
 
 LogicalResult LinalgDialect::verifyOperationAttribute(Operation *op,
