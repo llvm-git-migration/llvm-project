c7f9d45704fb1e70371baa60f025aa438b64bcd5
Hanbum Park <kese111@gmail.com>
2024-01-20 04:22:57 +0900

    [InstCombine] If Inst in unreachable refers to Inst change it to poison (#65107)
    Instructions in unreachable basic blocks are removed, but terminators
    are not. In this case, even instructions that are only referenced by
    a terminator, such as a return instruction, cannot be processed
    properly.
    
    This patch changes the operand of terminator instruction in an
    unreachable basic block to poison if it refers to the instruction,
    allowing the instruction to be properly processed.

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3231,9 +3231,8 @@ void InstCombinerImpl::handleUnreachableFrom(
     MadeIRChange = true;
   }
 
-  // RemoveDIs: to match behaviour in dbg.value mode, drop debug-info on
-  // terminator too.
-  BB->getTerminator()->dropDbgValues();
+  if (!DT.isReachableFromEntry(BB))
+    MadeIRChange |= handleUnreachableTerminator(BB->getTerminator());
 
   // Handle potentially dead successors.
   for (BasicBlock *Succ : successors(BB))
