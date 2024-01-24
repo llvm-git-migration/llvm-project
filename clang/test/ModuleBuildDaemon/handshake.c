1a9d7b89c9df03a683eab7db41ff8aebe13203bf
cpsughrue <cpsughrue@gmail.com>
2024-01-24 03:04:37 -0500

    Write lit macro for killing process

--- clang/test/ModuleBuildDaemon/handshake.c
+++ clang/test/ModuleBuildDaemon/handshake.c
@@ -1,16 +1,16 @@
 // Check that a clang invocation can spawn and handshake with a module build daemon
 // REQUIRES: !system-windows
 
-//  RUN: if pgrep -f "cc1modbuildd mbd-handshake"; then pkill -f "cc1modbuildd mbd-handshake"; fi
-//  RUN: rm -rf mbd-handshake %t
-//  RUN: split-file %s %t
+// RUN: %kill-process "-cc1modbuildd mbd-handshake"
+// RUN: rm -rf mbd-handshake %t
+// RUN: split-file %s %t
 
 //--- main.c
 int main() {return 0;}
 
 // RUN: %clang -fmodule-build-daemon=mbd-handshake -Rmodule-build-daemon %t/main.c &> %t/output-new || true
 // RUN: %clang -fmodule-build-daemon=mbd-handshake -Rmodule-build-daemon %t/main.c &> %t/output-existing || true
-// RUN: if pgrep -f "cc1modbuildd mbd-handshake"; then pkill -f "cc1modbuildd mbd-handshake"; fi
+// RUN: %kill-process "-cc1modbuildd mbd-handshake"
 
 // RUN: cat %t/output-new |  sed 's:\\\\\?:/:g' | FileCheck %s
 // RUN: cat %t/output-existing |  sed 's:\\\\\?:/:g' | FileCheck %s --check-prefix=CHECK-EXIST
