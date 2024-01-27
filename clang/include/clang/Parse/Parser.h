20b860d86e1db5981b6e362a11ac3ac437a4f007
yronglin <yronglin777@gmail.com>
2024-01-27 23:25:57 +0800

    [Clang] Implement P2718R0 "Lifetime extension in range-based for loops"
     Implement P2718R0 "Lifetime extension in range-based for loops" (https://wg21.link/P2718R0)
    
    Differential Revision: https://reviews.llvm.org/D153701

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -2398,7 +2398,7 @@ private:
   struct ForRangeInit {
     SourceLocation ColonLoc;
     ExprResult RangeExpr;
-
+    SmallVector<MaterializeTemporaryExpr *, 8> LifetimeExtendTemps;
     bool ParsedForRangeDecl() { return !ColonLoc.isInvalid(); }
   };
   struct ForRangeInfo : ForRangeInit {
