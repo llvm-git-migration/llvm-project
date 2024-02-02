834e55dbf7ac54ad7d006866ae4da3190f2197aa
houzhenyu <houzhenyu@huawei.com>
2024-02-02 11:21:50 +0800

    [X86] [iamcu] Fix wrong alignment value for attr (aligned) with -miamcu
    attribute ((aligned)) should be 4 for -miamcu.

--- clang/test/Sema/attr-aligned.c
+++ clang/test/Sema/attr-aligned.c
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin9 -fsyntax-only -verify %s
+// RUN: %clang_cc1 -triple i586-intel-elfiamcu -fsyntax-only -verify %s
 
 int x __attribute__((aligned(3))); // expected-error {{requested alignment is not a power of 2}}
 int y __attribute__((aligned(1ull << 33))); // expected-error {{requested alignment must be 4294967296 bytes or smaller}}
@@ -12,7 +13,11 @@ int y __attribute__((aligned(1 << 28)));
 
 // PR3254
 short g0[3] __attribute__((aligned));
+#ifdef __iamcu
+short g0_chk[__alignof__(g0) == 4 ? 1 : -1];
+#else
 short g0_chk[__alignof__(g0) == 16 ? 1 : -1];
+#endif
 
 typedef char ueber_aligned_char __attribute__((aligned(8)));
 
