5d07763edec7677a0b28935fea57976fb02f337a
Brandon Wu <brandon.wu@sifive.com>
2024-02-06 19:43:57 -0800

    fixup! [RISCV] RISCV vector calling convention (1/2)

--- clang/test/CodeGen/RISCV/riscv-vector-callingconv.c
+++ clang/test/CodeGen/RISCV/riscv-vector-callingconv.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -triple riscv64 -target-feature +v -verify
+// RUN: %clang_cc1 %s -std=c23 -triple riscv64 -target-feature +v -verify
 
 __attribute__((riscv_vector_cc)) int var; // expected-warning {{'riscv_vector_cc' only applies to function types; type here is 'int'}}
 
@@ -8,15 +8,6 @@ __attribute__((riscv_vector_cc(1))) void func_invalid(); // expected-error {{'ri
 void test_no_attribute(int); // expected-note {{previous declaration is here}}
 void __attribute__((riscv_vector_cc)) test_no_attribute(int x) { } // expected-error {{function declared 'riscv_vector_cc' here was previously declared without calling convention}}
 
-class test_cc {
-  __attribute__((riscv_vector_cc)) void member_func();
-};
-
-void test_lambda() {
-  __attribute__((riscv_vector_cc)) auto lambda = []() { // expected-warning {{'riscv_vector_cc' only applies to function types; type here is 'auto'}}
-  };
-}
-
 [[riscv::vector_cc]] int var2; // expected-warning {{'vector_cc' only applies to function types; type here is 'int'}}
 
 [[riscv::vector_cc]] void func2();
@@ -24,12 +15,3 @@ void test_lambda() {
 
 void test_no_attribute2(int); // expected-note {{previous declaration is here}}
 [[riscv::vector_cc]] void test_no_attribute2(int x) { } // expected-error {{function declared 'riscv_vector_cc' here was previously declared without calling convention}}
-
-class test_cc2 {
-  [[riscv::vector_cc]] void member_func();
-};
-
-void test_lambda2() {
-  [[riscv::vector_cc]] auto lambda = []() { // expected-warning {{'vector_cc' only applies to function types; type here is 'auto'}}
-  };
-}
