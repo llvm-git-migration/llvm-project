4103940b5a2578efc08df554ccdf80cb34925d09
Farzon Lotfi <farzon@farzon.org>
2024-02-08 11:08:59 -0500

    [HLSL] Implementation of dot intrinsic This change implements #70073
    HLSL has a dot intrinsic defined here:
    https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-dot
    
    The intrinsic itself is defined as a HLSL_LANG LangBuiltin in BuiltinsHLSL.td.
    This is used to associate all the dot product typdef defined hlsl_intrinsics.h
    with a single intrinsic check in CGBuiltin.cpp & SemaChecking.cpp.
    
    As a side note adding the dot product intrinsic to BuiltinsHLSL.td had a
    significant impact on re-compile time speeds. I recommend we move the
    other hlsl functions here. Further it lets us tap into the existing
    target specifc code organizations that exist in Sema and CodeGen.
    
    In IntrinsicsDirectX.td we define the llvmIR for the dot product.
    A few goals were in mind for this IR. First it should operate on only
    vectors. Second the return type should be the vector element type. Third
    the second parameter vector should be of the same size as the first
    parameter. Finally `a dot b` should be the same as `b dot a`.
    
    In CGBuiltin.cpp hlsl has built on top of existing clang intrinsics via EmitBuiltinExpr. Dot
    product though is a target specific intrinsic and so needed to establish
    a pattern for Target builtins via EmitDXILBuiltinExpr.
    The call chain looks like this now: EmitBuiltinExpr -> EmitTargetBuiltinExpr -> EmitTargetArchBuiltinExpr -> EmitDXILBuiltinExp
    
    EmitDXILBuiltinExp dot product intrinsics makes a destinction
    between vectors and scalars. This is because HLSL supports dot product on scalars which simplifies down to multiply.
    
    Sema.h & SemaChecking.cpp saw the addition of CheckHLSLBuiltinFunctionCall, a target specific semantic validation that can be expanded for other hlsl specific intrinsics.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -14017,6 +14017,7 @@ private:
   bool CheckPPCBuiltinFunctionCall(const TargetInfo &TI, unsigned BuiltinID,
                                    CallExpr *TheCall);
   bool CheckAMDGCNBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall);
+  bool CheckHLSLBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall);
   bool CheckRISCVLMUL(CallExpr *TheCall, unsigned ArgNum);
   bool CheckRISCVBuiltinFunctionCall(const TargetInfo &TI, unsigned BuiltinID,
                                      CallExpr *TheCall);
