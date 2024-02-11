18881f531078439d1df477b72f5ba0766cc55111
ergawy <kareem.ergawy@amd.com>
2024-02-11 07:57:53 -0600

    [MLIR][OpenMP] Support `llvm` conversion for `omp.private` regions
    Introduces conversion of `omp.private`'s regions to the LLVM dialect.
    This reuses the already existing conversion pattern for
    `ReducetionDeclareOp` and repurposes it to be used for multi-region ops
    as well.

--- mlir/lib/Dialect/OpenMP/IR/OpenMPDialect.cpp
+++ mlir/lib/Dialect/OpenMP/IR/OpenMPDialect.cpp
@@ -1594,6 +1594,15 @@ LogicalResult DataBoundsOp::verify() {
   return success();
 }
 
+void PrivateClauseOp::build(OpBuilder &odsBuilder, OperationState &odsState,
+                            TypeRange /*result_types*/, StringAttr symName,
+                            TypeAttr type) {
+  PrivateClauseOp::build(
+      odsBuilder, odsState, symName, type,
+      DataSharingClauseTypeAttr::get(odsBuilder.getContext(),
+                                     DataSharingClauseType::Private));
+}
+
 LogicalResult PrivateClauseOp::verify() {
   Type symType = getType();
 
