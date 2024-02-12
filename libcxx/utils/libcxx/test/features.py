4a7a186c82666ff57fc0f90aa1fd223c12f703b7
Hristo Hristov <hghristov.rmm@gmail.com>
2024-02-12 14:33:23 +0200

    Fix CI

--- libcxx/utils/libcxx/test/features.py
+++ libcxx/utils/libcxx/test/features.py
@@ -443,6 +443,7 @@ DEFAULT_FEATURES += [
     ),
 ]
 
+
 # Detect whether LLDB is on the system.
 def check_lldb(cfg):
     lldb_path = shutil.which("lldb")
@@ -459,6 +460,7 @@ DEFAULT_FEATURES += [
         actions=[AddSubstitution("%{lldb}", lambda cfg: shutil.which("lldb"))],
     )
 ]
+
 
 # Detect whether GDB is on the system, has Python scripting and supports
 # adding breakpoint commands. If so add a substitution to access it.
