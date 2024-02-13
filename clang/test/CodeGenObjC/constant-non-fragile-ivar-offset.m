7907980a315c86e43b14d1fc137fc655bbfd2dc3
Rose <83477269+AtariDreams@users.noreply.github.com>
2024-02-13 14:52:49 -0500

    [ObjC] Check entire chain of superclasses to see if class layout can be statically known
    As of now, we only check if a class directly inherits from NSObject to determine if said class has fixed offsets and can therefore have its memory layout statically known.
    
    However, if an NSObject subclass has fixed offsets, then so must the subclasses of that subclass, so this allows us to optimize instances of subclasses of subclasses that inherit from NSObject and so on.
    
    To determine this, we need to find that the compiler can see the implementation of each intermediate class, as that means it is statically linked.

--- clang/test/CodeGenObjC/constant-non-fragile-ivar-offset.m
+++ clang/test/CodeGenObjC/constant-non-fragile-ivar-offset.m
@@ -3,11 +3,11 @@
 // CHECK: @"OBJC_IVAR_$_StaticLayout.static_layout_ivar" = hidden constant i64 20
 // CHECK: @"OBJC_IVAR_$_SuperClass.superClassIvar" = hidden constant i64 20
 // CHECK: @"OBJC_IVAR_$_SuperClass._superClassProperty" = hidden constant i64 24
-// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar" = global i64 32
-// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar2" = global i64 40
-// CHECK: @"OBJC_IVAR_$_IntermediateClass._intermediateProperty" = hidden global i64 48
-// CHECK: @"OBJC_IVAR_$_SubClass.subClassIvar" = global i64 56
-// CHECK: @"OBJC_IVAR_$_SubClass._subClassProperty" = hidden global i64 64
+// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar" = constant i64 32
+// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar2" = constant i64 40
+// CHECK: @"OBJC_IVAR_$_IntermediateClass._intermediateProperty" = hidden constant i64 48
+// CHECK: @"OBJC_IVAR_$_SubClass.subClassIvar" = constant i64 56
+// CHECK: @"OBJC_IVAR_$_SubClass._subClassProperty" = hidden constant i64 64
 // CHECK: @"OBJC_IVAR_$_NotStaticLayout.not_static_layout_ivar" = hidden global i64 12
 
 @interface NSObject {
@@ -24,6 +24,7 @@
 -(void)meth {
   static_layout_ivar = 0;
   // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_StaticLayout
+  // CHECK: getelementptr inbounds i8, ptr %0, i64 20
 }
 @end
 
@@ -40,7 +41,7 @@
 - (void)superClassMethod {
     _superClassProperty = 42;
     superClassIvar = 10;
-    // CHECK: load i64, ptr @"OBJC_IVAR_$_SuperClass
+    // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_SuperClass
     // CHECK: getelementptr inbounds i8, ptr %1, i64 20
 }
 
@@ -67,8 +68,8 @@
 @synthesize intermediateProperty = _intermediateProperty;
 - (void)intermediateClassMethod {
     intermediateClassIvar = 3.14;
-    // CHECK: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
+    // CHECK: getelementptr inbounds i8, ptr %0, i64 32
 }
 
 // CHECK-LABEL: define internal void @"\01-[IntermediateClass intermediateClassPropertyMethod]"
@@ -81,7 +82,8 @@
 // CHECK-LABEL: define internal void @"\01-[IntermediateClass intermediateClassPropertyMethodDirect]"
 - (void)intermediateClassPropertyMethodDirect {
     _intermediateProperty = 0;
-    // CHECK: load i64, ptr @"OBJC_IVAR_$_IntermediateClass._intermediateProperty"
+    // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_IntermediateClass._intermediateProperty"
+    // CHECK: getelementptr inbounds i8, ptr %0, i64 48
 }
 @end
 
@@ -95,25 +97,26 @@
 @implementation SubClass
 // CHECK-LABEL: define internal void @"\01-[SubClass subclassVar]"
 - (void)subclassVar {
-    
     subClassIvar = 6.28;
-    // CHECK: load i64, ptr @"OBJC_IVAR_$_SubClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_SubClass
+    // CHECK: getelementptr inbounds i8, ptr %0, i64 56
 }
 
 // CHECK-LABEL: define internal void @"\01-[SubClass intermediateSubclassVar]"
 -(void)intermediateSubclassVar {
     intermediateClassIvar = 3.14;
-    // CHECK: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
+    // CHECK: getelementptr inbounds i8, ptr %0, i64 32
 }
 
 // implicit synthesized method here:
 // CHECK-LABEL: define internal ptr @"\01-[SubClass subClassProperty]"
-// CHECK: load i64, ptr @"OBJC_IVAR_$_SubClass._subClassProperty"
+// CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_SubClass._subClassProperty"
+// CHECK: getelementptr inbounds i8, ptr %0, i64 64
 
 // CHECK-LABEL: define internal void @"\01-[SubClass setSubClassProperty:]"
-// CHECK: load i64, ptr @"OBJC_IVAR_$_SubClass._subClassProperty"
+// CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_SubClass._subClassProperty"
+// CHECK: getelementptr inbounds i8, ptr %1, i64 64
 @end
 
 @interface NotNSObject {
