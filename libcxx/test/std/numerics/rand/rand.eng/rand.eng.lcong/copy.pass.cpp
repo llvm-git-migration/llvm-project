a91c6e0c748847fbe5d6dbd284b13b03e7366453
LRFLEW <LRFLEW@aol.com>
2024-02-13 01:19:04 -0600

    linear_congruential_engine: add using more precision to prevent overflow

--- libcxx/test/std/numerics/rand/rand.eng/rand.eng.lcong/copy.pass.cpp
+++ libcxx/test/std/numerics/rand/rand.eng/rand.eng.lcong/copy.pass.cpp
@@ -61,7 +61,6 @@ test()
   test1<T, A, M - 2, M>();
   test1<T, A, M - 1, M>();
 
-  /*
   // Cases where m is odd and m % a > m / a (not implemented)
   test1<T, M - 2, 0, M>();
   test1<T, M - 2, M - 2, M>();
@@ -69,7 +68,6 @@ test()
   test1<T, M - 1, 0, M>();
   test1<T, M - 1, M - 2, M>();
   test1<T, M - 1, M - 1, M>();
-  */
 }
 
 int main(int, char**)
