76eb88980c8ffc07b788809018f81a720de955c6
Rose <83477269+AtariDreams@users.noreply.github.com>
2024-02-13 14:52:49 -0500

    [ObjC] Check entire chain of superclasses to see if class layout can be statically known
    As of now, we only check if a class directly inherits from NSObject to determine if said class has fixed offsets and can therefore have its memory layout statically known.
    
    However, if an NSObject subclass has fixed offsets, then so must the subclasses of that subclass, so this allows us to optimize instances of subclasses of subclasses that inherit from NSObject and so on.
    
    To determine this, we need to find that the compiler can see the implementation of each intermediate class, as that means it is statically linked.

--- clang/test/CodeGenObjC/constant-non-fragile-ivar-offset.m
+++ clang/test/CodeGenObjC/constant-non-fragile-ivar-offset.m
@@ -3,10 +3,10 @@
 // CHECK: @"OBJC_IVAR_$_StaticLayout.static_layout_ivar" = hidden constant i64 20
 // CHECK: @"OBJC_IVAR_$_SuperClass.superClassIvar" = hidden constant i64 20
 // CHECK: @"OBJC_IVAR_$_SuperClass._superClassProperty" = hidden constant i64 24
-// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar" = global i64 32
-// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar2" = global i64 40
-// CHECK: @"OBJC_IVAR_$_IntermediateClass._intermediateProperty" = hidden global i64 48
-// CHECK: @"OBJC_IVAR_$_SubClass.subClassIvar" = global i64 56
+// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar" = constant i64 32
+// CHECK: @"OBJC_IVAR_$_IntermediateClass.intermediateClassIvar2" = constant i64 40
+// CHECK: @"OBJC_IVAR_$_IntermediateClass._intermediateProperty" = hidden constant i64 48
+// CHECK: @"OBJC_IVAR_$_SubClass.subClassIvar" = constant i64 56
 // CHECK: @"OBJC_IVAR_$_NotStaticLayout.not_static_layout_ivar" = hidden global i64 12
 
 @interface NSObject {
@@ -22,7 +22,7 @@
 }
 -(void)meth {
   static_layout_ivar = 0;
-  // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_StaticLayout
+  // CHECK: load i64, ptr @"OBJC_IVAR_$_StaticLayout
 }
 @end
 
@@ -38,7 +38,7 @@
     _superClassProperty = 42;
     superClassIvar = 10;
     // CHECK: load i64, ptr @"OBJC_IVAR_$_SuperClass
-    // CHECK: getelementptr inbounds i8, ptr %1, i64 20
+    // CHECK: getelementptr inbounds i8, ptr @"OBJC_IVAR_$_SuperClass
 }
 @end
 
@@ -58,12 +58,12 @@
 - (void)intermediateClassMethod {
     intermediateClassIvar = 3.14;
     // CHECK: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK: getelementptr inbounds i8, ptr @"OBJC_IVAR_$_IntermediateClass
 }
 
 - (void)intermediateClassPropertyMethod {
     self.intermediateProperty = 0;
-    // CHECK: getelementptr inbounds i8, ptr %2, i64 24
+    // CHECK: getelementptr inbounds i8, ptr @"OBJC_IVAR_$_IntermediateClass._intermediateProperty"
 }
 @end
 
@@ -78,14 +78,14 @@
     
     subClassIvar = 6.28;
     // CHECK: load i64, ptr @"OBJC_IVAR_$_SubClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK: getelementptr inbounds i8, ptr @"OBJC_IVAR_$_SubClass
 }
 
 -(void)intermediateSubclassVar
 {
     intermediateClassIvar = 3.14;
     // CHECK: load i64, ptr @"OBJC_IVAR_$_IntermediateClass
-    // CHECK: getelementptr inbounds i8, ptr %0, i64 %ivar
+    // CHECK: getelementptr inbounds i8, ptr @"OBJC_IVAR_$_IntermediateClass
 }
 
 @end
@@ -103,6 +103,6 @@
 }
 -(void)meth {
   not_static_layout_ivar = 0;
-  // CHECK: load i64, ptr @"OBJC_IVAR_$_NotStaticLayout.not_static_layout_ivar
+  // CHECK-NOT: load i64, ptr @"OBJC_IVAR_$_NotStaticLayout.not_static_layout_ivar
 }
 @end
