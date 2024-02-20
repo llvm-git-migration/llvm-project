153551af99c88d99398b870cf4155d250bb96c1e
Peiming Liu <peiming@google.com>
2024-02-20 18:22:49 +0000

    [mlir][sparse] support type conversion from AoS COO to memrefs.

--- mlir/include/mlir/Dialect/SparseTensor/IR/SparseTensorType.h
+++ mlir/include/mlir/Dialect/SparseTensor/IR/SparseTensorType.h
@@ -18,6 +18,17 @@
 namespace mlir {
 namespace sparse_tensor {
 
+/// A simple structure that encodes a range of levels in the sparse tensors that
+/// forms a COO segment.
+struct COOSegment {
+  std::pair<Level /*low*/, Level /*high*/> lvlRange;
+  bool isSoA;
+
+  bool inSegment(Level l) const {
+    return l >= lvlRange.first && l < lvlRange.second;
+  }
+};
+
 //===----------------------------------------------------------------------===//
 /// A wrapper around `RankedTensorType`, which has three goals:
 ///
@@ -329,6 +340,9 @@ public:
 
   /// Returns [un]ordered COO type for this sparse tensor type.
   RankedTensorType getCOOType(bool ordered) const;
+
+  /// Returns a list of COO segments in the sparse tensor types.
+  SmallVector<COOSegment> getCOOSegments() const;
 
 private:
   // These two must be const, to ensure coherence of the memoized fields.
