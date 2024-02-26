207631b1603673b16df00abbc880ab0c6f6c269e
Noah Goldstein <goldstein.w.n@gmail.com>
2024-02-26 15:54:04 -0600

    [Analysis] Move `DomConditionCache::findAffectedValues` to a new file; NFC

--- llvm/include/llvm/Analysis/ValueTracking.h
+++ llvm/include/llvm/Analysis/ValueTracking.h
@@ -1195,6 +1195,12 @@ std::optional<bool> isImpliedByDomCondition(CmpInst::Predicate Pred,
                                             const Value *LHS, const Value *RHS,
                                             const Instruction *ContextI,
                                             const DataLayout &DL);
+
+// Call `InsertAffected` on all Values whose known bits / value may be affected
+// the condition `Cond`. Used by AssumptionCache and DomConditionCache.
+void findValuesAffectedByCondition(Value *Cond, bool IsAssume,
+                                   function_ref<void(Value *)> InsertAffected);
+
 } // end namespace llvm
 
 #endif // LLVM_ANALYSIS_VALUETRACKING_H
