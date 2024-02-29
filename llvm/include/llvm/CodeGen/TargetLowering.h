ec1d57f4c38ed7bb328b9bdc95dc381c6fe5090a
miguelraz <miguelraz@ciencias.unam.mx>
2024-02-29 00:02:34 -0600

    add SDValue expandThreeWayCmp to TargetLowering.h

--- llvm/include/llvm/CodeGen/TargetLowering.h
+++ llvm/include/llvm/CodeGen/TargetLowering.h
@@ -5275,6 +5275,10 @@ public:
   /// method accepts integers as its arguments.
   SDValue expandAddSubSat(SDNode *Node, SelectionDAG &DAG) const;
 
+  /// Method for building the DAG expansion of ISD::[US]THREEWAYCMP. This
+  /// method accepts integers as its arguments.
+  SDValue expandThreeWayCmp(SDNode *Node, SelectionDAG &DAG) const;
+
   /// Method for building the DAG expansion of ISD::[US]SHLSAT. This
   /// method accepts integers as its arguments.
   SDValue expandShlSat(SDNode *Node, SelectionDAG &DAG) const;
