6fe180efa591e8282d7d9e72c6a7645ce34b2b74
wlei <wlei@fb.com>
2024-02-28 21:26:13 -0800

    Revert "Detect the order and add a summary flag to the profile"
    This reverts commit 28b0374b93f2072d03804ff1d8a8b2fd81c5a218.

--- llvm/lib/Transforms/IPO/SampleProfile.cpp
+++ llvm/lib/Transforms/IPO/SampleProfile.cpp
@@ -1190,7 +1190,7 @@ void SampleProfileLoader::findExternalInlineCandidate(
         CalleeSample->getContext().hasAttribute(ContextShouldBeInlined);
     if (!PreInline && CalleeSample->getHeadSamplesEstimate() < Threshold)
       continue;
-
+    
     Function *Func = SymbolMap.lookup(CalleeSample->getFunction());
     // Add to the import list only when it's defined out of module.
     if (!Func || Func->isDeclaration())
@@ -2172,14 +2172,6 @@ bool SampleProfileLoader::doInitialization(Module &M,
       Ctx.diagnose(DiagnosticInfoSampleProfile(M.getModuleIdentifier(), Msg,
                                                DS_Warning));
       return false;
-    }
-
-    if (!FunctionSamples::ProfileIsMixedProbeOrder) {
-      const char *Msg =
-          "Pseudo-probe-based profile is on an old version ID order which "
-          "could cause profile mismatch(performance regression)";
-      Ctx.diagnose(DiagnosticInfoSampleProfile(M.getModuleIdentifier(), Msg,
-                                               DS_Warning));
     }
   }
 
