9c49919b3a660153dd147a60a414710668cb6754
Bharadwaj Yadavalli <Bharadwaj.Yadavalli@microsoft.com>
2024-03-02 21:19:53 -0500

    [DirectX][NFC] Model precise overload type specification of DXIL Ops
    Implements an abstraction to specify precise overload types supported by
    DXIL ops. These overload types are typically a subset of LLVM
    intrinsics.
    
    Implements the corresponding changes in DXILEmitter backend.
    
    Adds tests to verify expected errors for unsupported overload types
    at code generation time.
    
    Add tests to check for correct overload error output.

--- llvm/lib/Target/DirectX/DXILOpBuilder.cpp
+++ llvm/lib/Target/DirectX/DXILOpBuilder.cpp
@@ -257,7 +257,7 @@ static FunctionCallee getOrCreateDXILOpFunction(dxil::OpCode DXILOp,
   // FIXME: find the issue and report error in clang instead of check it in
   // backend.
   if ((Prop->OverloadTys & (uint16_t)Kind) == 0) {
-    llvm_unreachable("invalid overload");
+    report_fatal_error("Invalid Overload Type", false);
   }
 
   std::string FnName = constructOverloadName(Kind, OverloadTy, *Prop);
