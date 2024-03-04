9434f834c4d48559aeec94403c927f48b15763e3
Craig Topper <craig.topper@sifive.com>
2024-03-04 11:24:34 -0800

    [RISCV] Add back SiFive's cdiscard.d.l1 and cflush.d.l1 instructions.
    These were in LLVM 17 but removed from LLVM 18 due to an incorrect
    extension name being used.
    
    This restores them with new extension names that match SiFive's
    downstream compiler. The extension name has been used internally
    for some time. It uses XSiFive instead of XSf like the newer extensions.
    
    The spec for the instructions is here https://sifive.cdn.prismic.io/sifive/767804da-53b2-4893-97d5-b7c030ae0a94_s76mc_core_complex_manual_21G3.pdf
    though the extension name is not listed.
    
    Column width in the extension printing had to be changed to accomodate
    a longer extension name.

--- llvm/lib/Support/RISCVISAInfo.cpp
+++ llvm/lib/Support/RISCVISAInfo.cpp
@@ -94,6 +94,8 @@ static const RISCVSupportedExtension SupportedExtensions[] = {
     {"xsfvfwmaccqqq", {1, 0}},
     {"xsfvqmaccdod", {1, 0}},
     {"xsfvqmaccqoq", {1, 0}},
+    {"xsifivecdiscarddlone", {1, 0}},
+    {"xsifivecflushdlone", {1, 0}},
     {"xtheadba", {1, 0}},
     {"xtheadbb", {1, 0}},
     {"xtheadbs", {1, 0}},
@@ -257,7 +259,7 @@ static void PrintExtension(StringRef Name, StringRef Version,
                            StringRef Description) {
   outs().indent(4);
   unsigned VersionWidth = Description.empty() ? 0 : 10;
-  outs() << left_justify(Name, 20) << left_justify(Version, VersionWidth)
+  outs() << left_justify(Name, 21) << left_justify(Version, VersionWidth)
          << Description << "\n";
 }
 
