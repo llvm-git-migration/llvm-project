d4b9d7559e8bdb8df85b63dc8b291658b8995319
Michael Jones <michaelrj@google.com>
2024-03-05 10:26:39 -0800

    [libc] add remaining epoll functions, pipe
    The epoll_wait functions need the rest of the epoll functions (create,
    ctl) to be available to actually test them, as well as pipe to create a
    usable file descriptor. This patch adds epoll_create, epoll_create1,
    epoll_ctl, and pipe. These have tests, and the tests for epoll_wait,
    epoll_pwait, and epoll_pwait2 (currently disabled) are updated to use
    these newly available functions.

--- libc/src/sys/epoll/linux/epoll_create.cpp
+++ libc/src/sys/epoll/linux/epoll_create.cpp
@@ -1,4 +1,4 @@
-//===---------- Linux implementation of the epoll_pwait function ----------===//
+//===---------- Linux implementation of the epoll_create function ---------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,28 +6,25 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/sys/epoll/epoll_pwait.h"
+#include "src/sys/epoll/epoll_create.h"
 
 #include "src/__support/OSUtil/syscall.h" // For internal syscall function.
 #include "src/__support/common.h"
 #include "src/errno/libc_errno.h"
 #include <sys/syscall.h> // For syscall numbers.
 
-// TODO: Use this include once the include headers are also using quotes.
-// #include "include/llvm-libc-types/sigset_t.h"
-// #include "include/llvm-libc-types/struct_epoll_event.h"
-
-#include <sys/epoll.h>
-#include <signal.h> // for NSIG
-
 namespace LIBC_NAMESPACE {
 
-LLVM_LIBC_FUNCTION(int, epoll_pwait,
-                   (int epfd, struct epoll_event *events, int maxevents,
-                    int timeout, const sigset_t *sigmask)) {
-  int ret = LIBC_NAMESPACE::syscall_impl<int>(
-      SYS_epoll_pwait, epfd, reinterpret_cast<long>(events), maxevents, timeout,
-      reinterpret_cast<long>(sigmask), NSIG / 8);
+LLVM_LIBC_FUNCTION(int, epoll_create, (int size)) {
+#ifdef SYS_epoll_create
+  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_epoll_create, size);
+#elif defined(SYS_epoll_create1)
+  // TODO: Silence warning for size being unused.
+  int ret = LIBC_NAMESPACE::syscall_impl<int>(SYS_epoll_create1, 0);
+#else
+#error                                                                         \
+    "epoll_create and epoll_create1 are unavailable. Unable to build epoll_create."
+#endif
 
   // A negative return value indicates an error with the magnitude of the
   // value being the error code.
