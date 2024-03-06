b8dddb0455b6087f1bf2b2da36ff49f16351881d
Alfie Richards <alfie.richards@arm.com>
2024-03-06 15:42:08 +0000

    Addressing minor comments.

--- llvm/utils/TableGen/CodeGenTarget.h
+++ llvm/utils/TableGen/CodeGenTarget.h
@@ -99,11 +99,6 @@ public:
   ///
   bool getAllowRegisterRenaming() const;
 
-  /// getPreferSmallerInstructions  - Return the PreferSmallerInstructions
-  /// flag value for this target.
-  ///
-  bool getPreferSmallerInstructions() const;
-
   /// getAsmParser - Return the AssemblyParser definition for this target.
   ///
   Record *getAsmParser() const;
