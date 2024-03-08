535ab47d3ce67f94be820c3fe2b5b67003db7100
Parth Arora <quic_partaror@quicinc.com>
2024-03-08 18:45:02 +0530

    [ELF] Fix unnecessary inclusion of unreferenced provide symbols
    Previously, linker was unnecessarily including a PROVIDE symbol which
    was referenced by another unused PROVIDE symbol. For example, if a
    linker script contained the below code and 'not_used_sym' provide symbol
    is not included, then linker was still unnecessarily including 'foo' PROVIDE
    symbol because it was referenced by 'not_used_sym'. This commit fixes
    this behavior.
    
    PROVIDE(not_used_sym = foo)
    PROVIDE(foo = 0x1000)
    
    This commit fixes this behavior by using dfs-like algorithm to find
    all the symbols referenced in provide expressions of included provide
    symbols.
    
    This commit also fixes the issue of unused section not being garbage-collected
    if a symbol of the section is referenced by an unused PROVIDE symbol.
    
    Closes #74771
    Closes #84730

---
+++ lld/test/ELF/gc-sections-with-provide.s
@@ -0,0 +1,60 @@
+# REQUIRES: x86
+
+# This test verifies that garbage-collection is correctly garbage collecting
+# unused sections when the symbol of the unused section is only referred by
+# an unused PROVIDE symbol.
+
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64 a.s -o a.o
+# RUN: ld.lld -o a_nogc a.o -T script.t
+# RUN: llvm-readobj --sections --symbols a_nogc | FileCheck -check-prefix=NOGC %s
+# RUN: ld.lld -o a_gc a.o --gc-sections --print-gc-sections -T script.t | FileCheck --check-prefix=GC_LINK %s
+# RUN: llvm-readobj --sections --symbols a_gc | FileCheck -check-prefix=GC %s
+
+NOGC: Name: foo
+NOGC: Name: used
+NOGC: Name: bar
+NOGC: Name: baz
+NOGC: Name: another_used
+NOGC: Name: baz_ref
+NOGC-NOT: unused
+NOGC-NOT: another_unused
+
+GC_LINK: removing unused section a.o:(.text.bar)
+
+GC: Name: foo
+GC: Name: used
+GC: Name: baz
+GC: Name: another_used
+GC: Name: baz_ref
+GC-NOT: bar
+GC-NOT: unused
+GC-NOT: another_unused
+
+#--- a.s
+.global _start
+_start:
+ call foo
+ call used
+
+.section .text.foo,"ax",@progbits
+foo:
+ nop
+
+.section .text.bar,"ax",@progbits
+.global bar
+bar:
+ nop
+
+.section .text.baz,"ax",@progbits
+.global baz
+baz:
+ nop
+
+
+#--- script.t
+PROVIDE(unused = bar + used);
+PROVIDE(used = another_used);
+PROVIDE(baz_ref = baz);
+PROVIDE(another_used = baz_ref);
+PROVIDE(another_unused = unused + bar + 0x1);
