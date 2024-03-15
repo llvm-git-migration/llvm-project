f9b6687bd1eafc87eaa7f47b4f0c2b19d53c5dd8
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-15 00:43:43 +0000

    [clang] Add `__has_extension(swiftcc)` support
    This patch adds `swiftcc` extension to check if the target supports Swift
    calling convention as well as we do for `swiftasynccc`.
    Also `swiftasynccc` is now considered to be a Clang extension rather than a
    language standard feature to reflect the nature of the attribute.

--- clang/test/Sema/swift-call-conv.c
+++ clang/test/Sema/swift-call-conv.c
@@ -1,7 +1,12 @@
 // RUN: %clang_cc1 -triple aarch64-unknown-windows-msvc -fsyntax-only %s -verify
 // RUN: %clang_cc1 -triple thumbv7-unknown-windows-msvc -fsyntax-only %s -verify
 // RUN: %clang_cc1 -triple x86_64-unknown-windows-msvc -fsyntax-only %s -verify
+// RISC-V does not support swiftcall
+// RUN: %clang_cc1 -triple riscv32-unknown-elf -fsyntax-only %s -verify
 
+#if __has_extension(swiftcc)
 // expected-no-diagnostics
-
+#else
+// expected-warning@+2 {{'__swiftcall__' calling convention is not supported for this target}}
+#endif
 void __attribute__((__swiftcall__)) f(void) {}
