4dc794da5c3bab65edce62e41e72569cdd3f04ff
Rose <gfunni234@gmail.com>
2024-03-17 15:31:39 -0400

    [SelectionDAG] Fold (avg x, 0) -> x >> 1

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -5083,7 +5083,10 @@ SDValue DAGCombiner::visitAVG(SDNode *N) {
   if (N0 == N1 && Level >= AfterLegalizeTypes)
     return N0;
 
-  // TODO If we use avg for scalars anywhere, we can add (avgfl x, 0) -> x >> 1
+  if (isNullOrNullSplat(N1) &&
+      (Opcode == ISD::AVGFLOORS || Opcode == ISD::AVGFLOORU))
+    return DAG.getNode((Opcode == ISD::AVGFLOORS) ? ISD::SRA : ISD::SRL, DL, VT,
+                       N0, DAG.getConstant(1, DL, VT));
 
   return SDValue();
 }
