a8f22b1a0f0105698caa85a754d5f366a75d1a52
Craig Topper <craig.topper@sifive.com>
2024-04-03 12:17:50 -0700

    fixup! update header comments.

--- llvm/lib/Target/RISCV/RISCVOptZba.cpp
+++ llvm/lib/Target/RISCV/RISCVOptZba.cpp
@@ -1,4 +1,4 @@
-//===- RISCVOptWInstrs.cpp - MI W instruction optimizations ---------------===//
+//===- RISCVOptZba.cpp - MI Zba instruction optimizations -----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -16,6 +16,8 @@
 // shift amounts.
 //
 // TODO: We can also support slli.uw by using shXadd.uw or the inner shXadd.
+//
+//===---------------------------------------------------------------------===//
 
 #include "RISCV.h"
 #include "RISCVSubtarget.h"
