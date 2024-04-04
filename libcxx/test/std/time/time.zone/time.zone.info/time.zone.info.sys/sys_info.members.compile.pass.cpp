c6445c9350461ea69774bf042616dab56a63efbf
Mark de Wever <koraq@xs4all.nl>
2024-04-04 20:14:54 +0200

    Addresses review comments.

--- libcxx/test/std/time/time.zone/time.zone.info/time.zone.info.sys/sys_info.members.compile.pass.cpp
+++ libcxx/test/std/time/time.zone/time.zone.info/time.zone.info.sys/sys_info.members.compile.pass.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
-// UNSUPPORTED: no-filesystem, no-localization, no-tzdb
 
 // XFAIL: libcpp-has-no-incomplete-tzdb
 
@@ -21,13 +20,19 @@
 //    string        abbrev;
 //  };
 
+// Validates whether:
+// - The members are present as non-const members.
+// - The struct is an aggregate.
+
 #include <chrono>
 #include <string>
+#include <type_traits>
 
 std::chrono::sys_info sys_info;
-
 [[maybe_unused]] std::chrono::sys_seconds& begin = sys_info.begin;
 [[maybe_unused]] std::chrono::sys_seconds& end   = sys_info.end;
 [[maybe_unused]] std::chrono::seconds& offset    = sys_info.offset;
 [[maybe_unused]] std::chrono::minutes& save      = sys_info.save;
 [[maybe_unused]] std::string& abbrev             = sys_info.abbrev;
+
+static_assert(std::is_aggregate_v<std::chrono::sys_info>);
