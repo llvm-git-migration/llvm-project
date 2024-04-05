a5703a8ade8b9bd00d474bdc12cd88b479fef67f
budimirarandjelovicsyrmia <budimir.arandjelovic@syrmia.com>
2024-04-05 15:20:37 +0200

    [clang] Catch missing format attributes

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -4640,6 +4640,8 @@ public:
 
   enum class RetainOwnershipKind { NS, CF, OS };
 
+  void DiagnoseMissingFormatAttributes(Stmt *Body, const FunctionDecl *FDecl);
+
   UuidAttr *mergeUuidAttr(Decl *D, const AttributeCommonInfo &CI,
                           StringRef UuidAsWritten, MSGuidDecl *GuidDecl);
 
