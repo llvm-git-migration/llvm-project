a368d066f5c54c5fbf555119a4a8844b6fd2cc72
Callum Fare <callum@codeplay.com>
2024-04-16 16:49:40 +0100

    [Offload] Introduce `offload-tblgen`

---
+++ offload/tools/offload-tblgen/Generators.hpp
@@ -0,0 +1,4 @@
+#include "llvm/TableGen/Record.h"
+
+void EmitOffloadAPI(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
+void EmitOffloadValidation(llvm::RecordKeeper &Records, llvm::raw_ostream &OS);
