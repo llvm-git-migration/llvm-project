f61686e42906886a0686158b3050767e60b576fa
Jinjin Li <lijinjin.868@bytedance.com>
2024-04-17 16:44:50 +0800

    [clang] Fix half && bfloat16 convert node expr codegen
    Data type conversion between fp16 and bf16 will generate fptrunc
    and fpextend nodes, but they are actually bitcast nodes.

--- clang/lib/CodeGen/CGExprScalar.cpp
+++ clang/lib/CodeGen/CGExprScalar.cpp
@@ -1431,9 +1431,13 @@ Value *ScalarExprEmitter::EmitScalarCast(Value *Src, QualType SrcType,
     return Builder.CreateFPToUI(Src, DstTy, "conv");
   }
 
-  if (DstElementTy->getTypeID() < SrcElementTy->getTypeID())
+  if ((DstElementTy->is16bitFPTy() && SrcElementTy->is16bitFPTy())) {
+    Value *FloatVal = Builder.CreateFPExt(Src, Builder.getFloatTy(), "conv");
+    return Builder.CreateFPTrunc(FloatVal, DstTy, "conv");
+  } else if (DstElementTy->getTypeID() < SrcElementTy->getTypeID())
     return Builder.CreateFPTrunc(Src, DstTy, "conv");
-  return Builder.CreateFPExt(Src, DstTy, "conv");
+  else
+    return Builder.CreateFPExt(Src, DstTy, "conv");
 }
 
 /// Emit a conversion from the specified type to the specified destination type,
@@ -1906,7 +1910,15 @@ Value *ScalarExprEmitter::VisitConvertVectorExpr(ConvertVectorExpr *E) {
   } else {
     assert(SrcEltTy->isFloatingPointTy() && DstEltTy->isFloatingPointTy() &&
            "Unknown real conversion");
-    if (DstEltTy->getTypeID() < SrcEltTy->getTypeID())
+    if ((DstEltTy->is16bitFPTy() && SrcEltTy->is16bitFPTy())) {
+      auto *ScrVecTy = cast<llvm::VectorType>(SrcTy);
+      Value *FloatVal = Builder.CreateFPExt(
+          Src,
+          llvm::VectorType::get(Builder.getFloatTy(),
+                                ScrVecTy->getElementCount()),
+          "conv");
+      Res = Builder.CreateFPTrunc(FloatVal, DstTy, "conv");
+    } else if (DstEltTy->getTypeID() < SrcEltTy->getTypeID())
       Res = Builder.CreateFPTrunc(Src, DstTy, "conv");
     else
       Res = Builder.CreateFPExt(Src, DstTy, "conv");
