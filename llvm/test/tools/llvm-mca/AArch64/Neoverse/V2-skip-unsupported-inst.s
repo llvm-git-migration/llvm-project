9e233531e3cd5a04b2a15c26bcfc4d8621665f97
Peter Waller <peter.waller@arm.com>
2024-04-22 10:04:15 +0100

    [llvm-mca] Add -skip-unsupported-instructions option
    Prior to this patch, if llvm-mca encountered an instruction which parses
    but has no scheduler info, the instruction is always reported as
    unsupported, and llvm-mca halts with an error.
    
    However, it would still be useful to allow MCA to continue even in the
    case of instructions lacking scheduling information. Obviously if
    scheduling information is lacking, it's not possible to give an accurate
    analysis for those instructions, and therefore a warning is emitted.
    
    A user could previously have worked around such unsupported instructions
    manually by deleting such instructions from the input, but this provides
    them a way of doing this for bulk inputs where they may not have a list
    of such unsupported instructions to drop up front.
    
    Note that this behaviour of instructions with no scheduling information
    under -skip-unsupported-instructions is analagous to current
    instructions which fail to parse: those are currently dropped from the
    input with a message printed, after which the analysis continues.

--- llvm/test/tools/llvm-mca/AArch64/Neoverse/V2-skip-unsupported-inst.s
+++ llvm/test/tools/llvm-mca/AArch64/Neoverse/V2-skip-unsupported-inst.s
@@ -1,22 +1,22 @@
-# RUN: llvm-mca -mtriple=x86_64-unknown-unknown -mcpu=btver2 -skip-unsupported-instructions %s |& FileCheck --check-prefix=CHECK-SKIP %s
-# RUN: not llvm-mca -mtriple=x86_64-unknown-unknown -mcpu=btver2 %s |& FileCheck --check-prefix=CHECK-ERROR %s
+# RUN: llvm-mca -march=aarch64 -mcpu=neoverse-v2 -skip-unsupported-instructions %s |& FileCheck --check-prefix=CHECK-SKIP %s
+# RUN: not llvm-mca -march=aarch64 -mcpu=neoverse-v2 %s |& FileCheck --check-prefix=CHECK-ERROR %s
 
 # CHECK-SKIP: warning: found an unsupported instruction in the input assembly sequence, skipping with -skip-unsupported-instructions, note accuracy will be impacted:
 # CHECK-ERROR: error: found an unsupported instruction in the input assembly sequence, use -skip-unsupported-instructions to ignore.
 
 # Currently lacks scheduling information and is therefore reported as unsupported by llvm-mca.
 # This may change some day in which case an alternative unsupported input would need to be found or the test removed.
-bzhi %eax, %ebx, %ecx
+steor x0, [x1]
 
-# Supported instruction that may be analysed.
-add %eax, %eax
+# Supported instruction that may be analysed
+add x0, x0, x0
 
 # CHECK-SKIP: Iterations:        100
 # CHECK-SKIP: Instructions:      100
 # CHECK-SKIP: Total Cycles:      103
 # CHECK-SKIP: Total uOps:        100
 
-# CHECK-SKIP: Dispatch Width:    2
+# CHECK-SKIP: Dispatch Width:    16
 # CHECK-SKIP: uOps Per Cycle:    0.97
 # CHECK-SKIP: IPC:               0.97
-# CHECK-SKIP: Block RThroughput: 0.5
+# CHECK-SKIP: Block RThroughput: 0.2
