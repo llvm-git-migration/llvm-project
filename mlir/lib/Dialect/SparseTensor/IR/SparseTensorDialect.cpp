e19fc6b62703418f7a4ee729e2ccc534be1551c9
Yinying Li <yinyingli@google.com>
2024-04-29 19:37:43 +0000

    remove redundant call

--- mlir/lib/Dialect/SparseTensor/IR/SparseTensorDialect.cpp
+++ mlir/lib/Dialect/SparseTensor/IR/SparseTensorDialect.cpp
@@ -921,7 +921,7 @@ LogicalResult SparseTensorEncodingAttr::verifyEncoding(
   }
   if (getImplicitVal()) {
     auto impVal = getImplicitVal();
-    if (auto typedAttr = llvm::dyn_cast<TypedAttr>(getImplicitVal())) {
+    if (auto typedAttr = llvm::dyn_cast<TypedAttr>(impVal)) {
       Type attrType = typedAttr.getType();
       if (attrType != elementType) {
         return emitError()
