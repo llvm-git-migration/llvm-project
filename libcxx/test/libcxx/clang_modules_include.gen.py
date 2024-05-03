72e68245564646d52175c7793f1eaceb7fd58bb8
Jake Egan <Jake.egan@ibm.com>
2024-05-03 10:35:29 -0400

    [AIX][libc++] Enable clang_modules_include.gen.py tests
    Enable these tests on AIX since they're passing.

--- libcxx/test/libcxx/clang_modules_include.gen.py
+++ libcxx/test/libcxx/clang_modules_include.gen.py
@@ -30,9 +30,6 @@ for header in public_headers:
 // UNSUPPORTED{BLOCKLIT}: windows
 // UNSUPPORTED{BLOCKLIT}: buildhost=windows
 
-// The AIX headers don't appear to be compatible with modules
-// UNSUPPORTED{BLOCKLIT}: LIBCXX-AIX-FIXME
-
 // The Android headers don't appear to be compatible with modules yet
 // UNSUPPORTED{BLOCKLIT}: LIBCXX-ANDROID-FIXME
 
@@ -59,9 +56,6 @@ print(f"""\
 // The Windows headers don't appear to be compatible with modules
 // UNSUPPORTED{BLOCKLIT}: windows
 // UNSUPPORTED{BLOCKLIT}: buildhost=windows
-
-// The AIX headers don't appear to be compatible with modules
-// UNSUPPORTED{BLOCKLIT}: LIBCXX-AIX-FIXME
 
 // The Android headers don't appear to be compatible with modules yet
 // UNSUPPORTED{BLOCKLIT}: LIBCXX-ANDROID-FIXME
