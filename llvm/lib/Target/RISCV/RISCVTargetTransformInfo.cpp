f7da97d17b49c648337620250fb3b841dfe5434e
Elvis Wang <elvis.wang@sifive.com>
2024-05-05 18:28:24 -0700

    [RISCV][CostModel] Remove inst cost of cmp inst in cmp-select sequence.
    Scalar ICmp and scalar Select instructinos will lower to SELECT_CC and lower to PseudoCCMOVGPR
    which will generate a conditional branch instr and a move instr.
    The cost of scalar (ICmp + Select) = (0 + Select instr cost).

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -1443,6 +1443,15 @@ InstructionCost RISCVTTIImpl::getCmpSelInstrCost(unsigned Opcode, Type *ValTy,
     }
   }
 
+  // The scalar icmp + select instructions will lower to SELECT_CC and lower to
+  // PseudoCCMOVGPR which will generate a conditional branch + mv. The
+  // cost of (icmp + select) will be (0 + select instr cost).
+  if (I && isa<ICmpInst>(I) && !I->getType()->isVectorTy() && I->hasOneUser() &&
+      isa<SelectInst>(I->user_back()) &&
+      !I->user_back()->getType()->isVectorTy() &&
+      I->user_back()->getOperand(0) == I)
+    return 0;
+
   // TODO: Add cost for scalar type.
 
   return BaseT::getCmpSelInstrCost(Opcode, ValTy, CondTy, VecPred, CostKind, I);
