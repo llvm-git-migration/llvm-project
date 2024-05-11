16a00f4cf511e6dd96202d3013b41873f8dcba6b
Chris Copeland <chris@chrisnc.net>
2024-05-11 00:15:50 -0700

    [ARM][clang] Fix warning for VFP function calls from interrupts.
    This warning has two issues:
     - The interrupt attribute doesn't only change how volatile registers
       are treated; it also causes the function to return using an exception
       return instruction. This warning allows calls from one function with
       the interrupt attribute to another, and the diagnostic text suggests
       that not having the attribute on the callee is the problem. Actually
       making such a call will lead to a double exception return, which is
       unpredictable according to the ARM architecture manual section
       B9.1.1, "Restrictions on exception return instructions". Even on
       machines where an exception return from user/system mode is
       tolerated, if the callee's interrupt type is anything other than a
       supervisor call or secure monitor call, it will also return to a
       different address than a normal function would. For example,
       returning from an "IRQ" handler will return to lr - 4, which will
       generally result in calling the same function again.
     - It is part of the -Wextra diagnostic group and can't be individually
       disabled when using -Wextra, which also means the diagnostic text of
       this specific warning appears in the documentation of -Wextra.
    
    This change addresses both issues. Rather than check that the callee has
    the interrupt attribute, check that it uses the soft-float feature,
    which will prevent use of VFP state. The warning is also given its own
    diagnostic group.
    
    Closes #34876.

--- clang/test/Sema/arm-interrupt-attr.c
+++ clang/test/Sema/arm-interrupt-attr.c
@@ -1,7 +1,7 @@
-// RUN: %clang_cc1 %s -triple arm-apple-darwin  -target-feature +vfp2 -verify -fsyntax-only
-// RUN: %clang_cc1 %s -triple thumb-apple-darwin  -target-feature +vfp3 -verify -fsyntax-only
-// RUN: %clang_cc1 %s -triple armeb-none-eabi  -target-feature +vfp4 -verify -fsyntax-only
-// RUN: %clang_cc1 %s -triple thumbeb-none-eabi  -target-feature +neon -verify -fsyntax-only
+// RUN: %clang_cc1 %s -triple arm-apple-darwin -target-feature +vfp2 -verify -fsyntax-only
+// RUN: %clang_cc1 %s -triple thumb-apple-darwin -target-feature +vfp3 -verify -fsyntax-only
+// RUN: %clang_cc1 %s -triple armeb-none-eabi -target-feature +vfp4 -verify -fsyntax-only
+// RUN: %clang_cc1 %s -triple thumbeb-none-eabi -target-feature +neon -verify -fsyntax-only
 // RUN: %clang_cc1 %s -triple thumbeb-none-eabi -target-feature +neon -target-feature +soft-float -DSOFT -verify -fsyntax-only
 
 __attribute__((interrupt(IRQ))) void foo(void) {} // expected-error {{'interrupt' attribute requires a string}}
@@ -23,7 +23,7 @@ __attribute__((interrupt(""))) void foo10(void) {}
 // expected-note@+2 {{'callee1' declared here}}
 #endif
 void callee1(void);
-__attribute__((interrupt("IRQ"))) void callee2(void);
+__attribute__((target("soft-float"))) void callee2(void);
 void caller1(void) {
   callee1();
   callee2();
@@ -31,13 +31,13 @@ void caller1(void) {
 
 #ifndef SOFT
 __attribute__((interrupt("IRQ"))) void caller2(void) {
-  callee1(); // expected-warning {{call to function without interrupt attribute could clobber interruptee's VFP registers}}
+  callee1(); // expected-warning {{calling a function from an interrupt handler could clobber the interruptee's VFP registers if the callee also uses VFP}}
   callee2();
 }
 
 void (*callee3)(void);
 __attribute__((interrupt("IRQ"))) void caller3(void) {
-  callee3(); // expected-warning {{call to function without interrupt attribute could clobber interruptee's VFP registers}}
+  callee3(); // expected-warning {{calling a function from an interrupt handler could clobber the interruptee's VFP registers if the callee also uses VFP}}
 }
 #else
 __attribute__((interrupt("IRQ"))) void caller2(void) {
