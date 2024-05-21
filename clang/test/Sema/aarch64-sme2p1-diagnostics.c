9df985b40150404d5037e8752765f68e18c77e79
Tomas Matheson <tomas.matheson@arm.com>
2024-05-21 09:34:56 +0100

    clang/test changes to add missing sme/sve dependencies

--- clang/test/Sema/aarch64-sme2p1-diagnostics.c
+++ clang/test/Sema/aarch64-sme2p1-diagnostics.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple aarch64-none-linux-gnu  -target-feature +sme2p1 -fsyntax-only -verify %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu  -target-feature +sme2p1 -target-feature +sme2 -fsyntax-only -verify %s
 
 // REQUIRES: aarch64-registered-target
 #include "arm_sme.h"
