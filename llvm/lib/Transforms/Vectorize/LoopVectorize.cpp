f0150b232596b7fc263c1660613a8f1b9f4f7730
Mel Chen <mel.chen@sifive.com>
2024-05-23 00:20:20 -0700

    Refine the code

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -8731,10 +8731,11 @@ LoopVectorizationPlanner::tryToBuildVPlanWithVPRecipes(VFRange &Range) {
       StoreInst *SI;
       if ((SI = dyn_cast<StoreInst>(&I)) &&
           Legal->isInvariantAddressOfReduction(SI->getPointerOperand())) {
-	// Only create recipe for the last intermediate store.
+        // Only create recipe for the last intermediate store of the reduction.
         if (Legal->isInvariantStoreOfReduction(SI)) {
           auto *Recipe = new VPIntermediateStoreRecipe(
-              I, /* StoredVal = */ Operands[0], /* Address = */ Operands[1]);
+              *SI, /* StoredVal = */ Operands[0], /* Address = */ Operands[1],
+              SI->getDebugLoc());
           MiddleVPBB->appendRecipe(Recipe);
         }
         continue;
