b063ea3a65d1674dcad639194c052668bc3b2fd3
Mital Ashok <mital@mitalashok.co.uk>
2024-05-25 07:57:09 +0100

    [libc++] Cooperation between std::optional and other standard types
    As a proof of concept, support std::optional<std::reference_wrapper<T>> and make it have a similar ABI to T*.

---
+++ libcxx/test/libcxx/utilities/optional/cooperate/optional.object/optional.object.observe/bool.pass.cpp
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// <optional>
+
+// constexpr explicit optional<T>::operator bool() const noexcept;
+
+#include <optional>
+#include <functional>
+#include <type_traits>
+#include <cassert>
+
+#include "test_macros.h"
+
+using std::optional;
+
+template <typename T, typename F>
+constexpr bool test(F init) {
+  {
+    const optional<T> opt;
+    (void)opt;
+    ASSERT_NOEXCEPT(bool(opt));
+    static_assert(!std::is_convertible_v<optional<T>, bool>);
+  }
+  {
+    constexpr optional<T> opt;
+    static_assert(!opt);
+  }
+  {
+    constexpr optional<T> opt(init());
+    static_assert(opt);
+  }
+  return true;
+}
+
+int f() { return 0; }
+
+int main(int, char**) {
+  static int i;
+  test<std::reference_wrapper<int>>([]() -> auto& { return i; });
+  test<std::reference_wrapper<int()>>([]() -> auto& { return f; });
+}
