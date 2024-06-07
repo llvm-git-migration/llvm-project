4e1625c0fbcafe73034a91c889ce06305273b676
Stephen Tozer <stephen.tozer@sony.com>
2024-06-07 15:36:09 +0100

    Add pass to remove loads that only feed into FAKE_USEs

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -1269,6 +1269,8 @@ void TargetPassConfig::addMachinePasses() {
 
   PM->add(createStackFrameLayoutAnalysisPass());
 
+  addPass(&RemoveLoadsIntoFakeUsesID);
+
   // Add passes that directly emit MI after all other MI passes.
   addPreEmitPass2();
 
