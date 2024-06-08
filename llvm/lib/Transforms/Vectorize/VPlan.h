d24d854d9169bb0e88f3b2bd991e94bef24a7824
Mel Chen <mel.chen@sifive.com>
2024-06-08 10:28:11 -0700

    Introduce ExplicitVectorLengthMask recipe for out-loop reduction.

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1192,6 +1192,7 @@ public:
     SLPStore,
     ActiveLaneMask,
     ExplicitVectorLength,
+    ExplicitVectorLengthMask,
     CalculateTripCountMinusVF,
     // Increment the canonical IV separately for each unrolled part.
     CanonicalIVIncrementForPart,
