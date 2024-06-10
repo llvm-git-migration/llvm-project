8ad1b50f267b24693189e3a401d5970308a09f83
Qizhi Hu <836744285@qq.com>
2024-06-10 16:53:54 +0800

    [clang] SourceLocIdentKind::Function should not be dependent

--- clang/include/clang/AST/Expr.h
+++ clang/include/clang/AST/Expr.h
@@ -4786,7 +4786,6 @@ public:
 
   static bool MayBeDependent(SourceLocIdentKind Kind) {
     switch (Kind) {
-    case SourceLocIdentKind::Function:
     case SourceLocIdentKind::FuncSig:
     case SourceLocIdentKind::SourceLocStruct:
       return true;
