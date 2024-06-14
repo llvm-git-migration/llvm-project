0a95ea61166e8674abda20c440e55ae1a1196e1f
Lucas Prates <lucas.prates@arm.com>
2024-06-14 14:22:01 +0100

    [NFC][AArch64] Rename TargetParser's ExtensionInfo fields
    This renames some of the fields in AArchte64TargetParser's ExtensionInfo
    struct to better reflect their use cases and improve readability.

--- llvm/lib/TargetParser/AArch64TargetParser.cpp
+++ llvm/lib/TargetParser/AArch64TargetParser.cpp
@@ -58,8 +58,8 @@ bool AArch64::getExtensionFeatures(
     std::vector<StringRef> &Features) {
   for (const auto &E : Extensions)
     /* INVALID and NONE have no feature name. */
-    if (InputExts.test(E.ID) && !E.Feature.empty())
-      Features.push_back(E.Feature);
+    if (InputExts.test(E.ID) && !E.TargetFeature.empty())
+      Features.push_back(E.TargetFeature);
 
   return true;
 }
@@ -77,7 +77,7 @@ StringRef AArch64::getArchExtFeature(StringRef ArchExt) {
 
   if (auto AE = parseArchExtension(ArchExtBase)) {
     // Note: the returned string can be empty.
-    return IsNegated ? AE->NegFeature : AE->Feature;
+    return IsNegated ? AE->NegTargetFeature : AE->TargetFeature;
   }
 
   return StringRef();
@@ -113,9 +113,9 @@ const AArch64::ArchInfo *AArch64::parseArch(StringRef Arch) {
 std::optional<AArch64::ExtensionInfo>
 AArch64::parseArchExtension(StringRef ArchExt) {
   for (const auto &A : Extensions) {
-    if (A.Name.empty() && !A.Alias)
+    if (A.UserVisibleName.empty() && !A.Alias)
       continue;
-    if (ArchExt == A.Name || ArchExt == A.Alias)
+    if (ArchExt == A.UserVisibleName || ArchExt == A.Alias)
       return A;
   }
   return {};
@@ -124,7 +124,7 @@ AArch64::parseArchExtension(StringRef ArchExt) {
 std::optional<AArch64::ExtensionInfo>
 AArch64::targetFeatureToExtension(StringRef TargetFeature) {
   for (const auto &E : Extensions)
-    if (TargetFeature == E.Feature)
+    if (TargetFeature == E.TargetFeature)
       return E;
   return {};
 }
@@ -147,11 +147,11 @@ void AArch64::PrintSupportedExtensions(StringMap<StringRef> DescMap) {
          << (DescMap.empty() ? "\n" : "Description\n");
   for (const auto &Ext : Extensions) {
     // Extensions without a feature cannot be used with -march.
-    if (!Ext.Name.empty() && !Ext.Feature.empty()) {
-      std::string Description = DescMap[Ext.Name].str();
+    if (!Ext.UserVisibleName.empty() && !Ext.TargetFeature.empty()) {
+      std::string Description = DescMap[Ext.UserVisibleName].str();
       outs() << "    "
              << format(Description.empty() ? "%s\n" : "%-20s%s\n",
-                       Ext.Name.str().c_str(), Description.c_str());
+                       Ext.UserVisibleName.str().c_str(), Description.c_str());
     }
   }
 }
@@ -253,7 +253,7 @@ bool AArch64::ExtensionSet::parseModifier(StringRef Modifier,
   StringRef ArchExt = Modifier.drop_front(NChars);
 
   if (auto AE = parseArchExtension(ArchExt)) {
-    if (AE->Feature.empty() || AE->NegFeature.empty())
+    if (AE->TargetFeature.empty() || AE->NegTargetFeature.empty())
       return false;
     if (IsNegated)
       disable(AE->ID);
