cceb89dbb820e3b8db8e4997ddc1b08a06f42cdc
Mel Chen <mel.chen@sifive.com>
2024-06-13 23:22:51 -0700

    Revert "Introduce ExplicitVectorLengthMask recipe for out-loop reduction."
    This reverts commit 8488520439de8814cfcfa0843f22673c6aabde70.

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1183,7 +1183,6 @@ public:
     SLPStore,
     ActiveLaneMask,
     ExplicitVectorLength,
-    ExplicitVectorLengthMask,
     CalculateTripCountMinusVF,
     // Increment the canonical IV separately for each unrolled part.
     CanonicalIVIncrementForPart,
