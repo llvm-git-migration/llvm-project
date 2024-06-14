170f491f6c153abc3c94a6dd2ccde7b6fe6d3969
Lucas Prates <lucas.prates@arm.com>
2024-06-14 16:54:39 +0100

    [NFC][AArch64] Make extension descriptions available in TargetParser
    This introduces a new field to AArch64TargetParser's ExtensionInfo
    struct that contains the extension description from the TableGen record.
    This allows us to simplify the information flow for functionality such
    as '--print-supported-extensions'.

--- llvm/utils/TableGen/ARMTargetDefEmitter.cpp
+++ llvm/utils/TableGen/ARMTargetDefEmitter.cpp
@@ -101,6 +101,7 @@ static void EmitARMTargetDef(RecordKeeper &RK, raw_ostream &OS) {
     else
       OS << ", \"" << Alias << "\"";
     OS << ", AArch64::" << AEK;
+    OS << ", \"" << Rec->getValueAsString("Desc") << "\"";
     if (AEK == "AEK_NONE") {
       // HACK: don't emit posfeat/negfeat strings for FMVOnlyExtensions.
       OS << ", {}, {}";
@@ -113,7 +114,7 @@ static void EmitARMTargetDef(RecordKeeper &RK, raw_ostream &OS) {
     OS << ", " << (uint64_t)Rec->getValueAsInt("FMVPriority");
     OS << "},\n";
   };
-  OS << "  {\"none\", {}, AArch64::AEK_NONE, {}, {}, FEAT_INIT, \"\", "
+  OS << "  {\"none\", {}, AArch64::AEK_NONE, {}, {}, {}, FEAT_INIT, \"\", "
         "ExtensionInfo::MaxFMVPriority},\n";
   OS << "};\n"
      << "#undef EMIT_EXTENSIONS\n"
