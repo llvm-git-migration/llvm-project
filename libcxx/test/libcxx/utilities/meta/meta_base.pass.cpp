76238f0de9de2724ce68b4e1bd0aec95c1bfcbd4
Nikolas Klauser <nikolasklauser@berlin.de>
2024-06-19 09:23:31 +0200

    [libc++] Include the rest of the detail headers by version in the umbrella headers

--- libcxx/test/libcxx/utilities/meta/meta_base.pass.cpp
+++ libcxx/test/libcxx/utilities/meta/meta_base.pass.cpp
@@ -10,9 +10,11 @@
 #include "test_macros.h"
 
 TEST_CLANG_DIAGNOSTIC_IGNORED("-Wprivate-header")
+#include <__type_traits/conjunction.h>
+#include <__type_traits/disjunction.h>
 #include <__type_traits/is_valid_expansion.h>
-#include <type_traits>
 #include <cassert>
+#include <type_traits>
 
 struct Bomb;
 template <int N, class T = Bomb >
