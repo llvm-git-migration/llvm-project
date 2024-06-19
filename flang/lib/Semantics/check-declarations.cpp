d45672f222287b737b3501fb88bcbd20754ce2b4
Peter Klausler <pklausler@nvidia.com>
2024-06-19 12:46:30 -0700

    [flang] Silence errors on C_LOC/C_FUNLOC in specification expressions
    Transformational functions from the intrinsic module ISO_C_BINDING are allowed
    in specification expressions, so tweak some general checks that would otherwise
    trigger error messages about inadmissible targets, dummy procedures in specification
    expressions, and pure procedures with impure dummy procedures.

--- flang/lib/Semantics/check-declarations.cpp
+++ flang/lib/Semantics/check-declarations.cpp
@@ -354,7 +354,10 @@ void CheckHelper::Check(const Symbol &symbol) {
       messages_.Say(
           "A pure subprogram may not have a variable with the VOLATILE attribute"_err_en_US);
     }
-    if (IsProcedure(symbol) && !IsPureProcedure(symbol) && IsDummy(symbol)) {
+    if (innermostSymbol_ && innermostSymbol_->name() == "__builtin_c_funloc") {
+      // The intrinsic procedure C_FUNLOC() gets a pass on this check.
+    } else if (IsProcedure(symbol) && !IsPureProcedure(symbol) &&
+        IsDummy(symbol)) {
       messages_.Say(
           "A dummy procedure of a pure subprogram must be pure"_err_en_US);
     }
