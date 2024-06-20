d5c696a30c1cf7aed0b339680865ed5071c0659e
Kolya Panchenko <kolya.panchenko@sifive.com>
2024-06-20 13:12:58 -0700

    Moved clone() methods to unreachable for EVL-recipes

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -1399,12 +1399,8 @@ public:
   ~VPWidenEVLRecipe() override = default;
 
   VPWidenRecipe *clone() override final {
-    SmallVector<VPValue *> Ops(operands());
-    VPValue *EVL = Ops.pop_back_val();
-    auto *R = new VPWidenEVLRecipe(*getUnderlyingInstr(),
-                                   make_range(Ops.begin(), Ops.end()), *EVL);
-    R->transferFlags(*this);
-    return R;
+    llvm_unreachable("VPWidenStoreEVLRecipe cannot be cloned");
+    return nullptr;
   }
 
   VP_CLASSOF_IMPL(VPDef::VPWidenEVLSC);
@@ -2489,6 +2485,11 @@ struct VPWidenLoadEVLRecipe final : public VPWidenMemoryRecipe, public VPValue {
     setMask(Mask);
   }
 
+  VPWidenLoadEVLRecipe *clone() override {
+    llvm_unreachable("VPWidenLoadEVLRecipe recipe cannot be cloned");
+    return nullptr;
+  }
+
   VP_CLASSOF_IMPL(VPDef::VPWidenLoadEVLSC)
 
   /// Return the EVL operand.
@@ -2563,6 +2564,11 @@ struct VPWidenStoreEVLRecipe final : public VPWidenMemoryRecipe {
                             S->isConsecutive(), S->isReverse(),
                             S->getDebugLoc()) {
     setMask(Mask);
+  }
+
+  VPWidenStoreEVLRecipe *clone() override {
+    llvm_unreachable("VPWidenStoreEVLRecipe cannot be cloned");
+    return nullptr;
   }
 
   VP_CLASSOF_IMPL(VPDef::VPWidenStoreEVLSC)
