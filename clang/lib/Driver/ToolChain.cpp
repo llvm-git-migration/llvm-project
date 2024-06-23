25519e89cad1676cbabaf0d9a887397777626b63
trcrsired <uwgghhbcad@gmail.com>
2024-06-23 00:07:19 -0400

    Support --sysroot= for ${arch}-windows-msvc targets
    I think it is possible to use the same rule for msvc targets with
    --target= and --sysroot=
    
    See Repository:
    https://github.com/trcrsired/windows-msvc-sysroot
    
    Add sysroot support for msvc
    
    MSVC.cpp needs getDriver before using D
    
    add stl in parser for -stdlib=
    
    Add Vcruntime to runtime list and unwind list
    
    MSVC add default runtime lib type and default unwind lib type
    
    add a msvc sysroot test
    
    use %S instead of /foo
    
    Fix test for msvc-sysroot
    
    Also add a pesudo implementation for WebAssembly and
    maybe Microsoft STL could be ported to more targets in the future
    
    Fix the toggle of wasm that prevents -stdlib=stl passed into
    
    Avoid clang-formatting in MSVC.cpp
    
    Add some comments to ToolChain
    
    avoid indent the if block
    
    Add back space before winsysroot line
    
    use  instead of arch in the comment
    
    remove FIXME for libc++ since we have added the logic here
    
    Remove MSVC.h formatting
    
    Remove default cases in WebAssembly
    
    add back the empty line before the Sysroot line
    
    fix msvc-sysroot typo for libc++ loongarch
    
    Fix : missing in CST_Stl case

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1112,6 +1112,8 @@ ToolChain::RuntimeLibType ToolChain::GetRuntimeLibType(
     runtimeLibType = ToolChain::RLT_CompilerRT;
   else if (LibName == "libgcc")
     runtimeLibType = ToolChain::RLT_Libgcc;
+  else if (LibName == "vcruntime")
+    runtimeLibType = ToolChain::RLT_Vcruntime;
   else if (LibName == "platform")
     runtimeLibType = GetDefaultRuntimeLibType();
   else {
@@ -1150,6 +1152,8 @@ ToolChain::UnwindLibType ToolChain::GetUnwindLibType(
     unwindLibType = ToolChain::UNW_CompilerRT;
   } else if (LibName == "libgcc")
     unwindLibType = ToolChain::UNW_Libgcc;
+  else if (LibName == "vcruntime")
+    unwindLibType = ToolChain::UNW_Vcruntime;
   else {
     if (A)
       getDriver().Diag(diag::err_drv_invalid_unwindlib_name)
@@ -1173,6 +1177,8 @@ ToolChain::CXXStdlibType ToolChain::GetCXXStdlibType(const ArgList &Args) const{
     cxxStdlibType = ToolChain::CST_Libcxx;
   else if (LibName == "libstdc++")
     cxxStdlibType = ToolChain::CST_Libstdcxx;
+  else if (LibName == "stl")
+    cxxStdlibType = ToolChain::CST_Stl;
   else if (LibName == "platform")
     cxxStdlibType = GetDefaultCXXStdlibType();
   else {
@@ -1310,6 +1316,10 @@ void ToolChain::AddCXXStdlibLibArgs(const ArgList &Args,
 
   case ToolChain::CST_Libstdcxx:
     CmdArgs.push_back("-lstdc++");
+    break;
+
+  case ToolChain::CST_Stl:
+    // MSVC STL does not need to add -l
     break;
   }
 }
