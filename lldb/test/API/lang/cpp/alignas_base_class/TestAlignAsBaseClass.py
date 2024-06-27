2113f052dd69cc1773f642e06339e5a13599090c
Michael Buch <michaelbuch12@gmail.com>
2024-06-27 17:16:50 +0100

    [lldb][test] Add test-cases for packed structures
    Adds test that checks that LLDB correctly infers the
    alignment of packed structures. Specifically, the
    `InferAlignment` code-path of the `ItaniumRecordLayoutBuilder`
    where it assumes that overlapping field offsets imply a
    packed structure and thus sets alignment to `1`. See discussion
    in https://github.com/llvm/llvm-project/pull/93809.
    
    Also adds two XFAIL-ed tests:
    1. where LLDB doesn't correctly infer the alignment of a derived class whose base has
    an explicit `DW_AT_alignment. See https://github.com/llvm/llvm-project/issues/73623.
    2. where the aforementioned `InferAlignment` kicks in for
       overlapping fields (but in this case incorrectly since
       the structure isn't actually packed).

--- lldb/test/API/lang/cpp/alignas_base_class/TestAlignAsBaseClass.py
+++ lldb/test/API/lang/cpp/alignas_base_class/TestAlignAsBaseClass.py
@@ -16,3 +16,11 @@ class TestCase(TestBase):
         # Verify specified class alignments.
         self.expect_expr("alignof(B2)", result_value="8")
         self.expect_expr("alignof(EmptyClassAlign8)", result_value="8")
+
+    @no_debug_info_test
+    @expectedFailureAll(bugnumber="https://github.com/llvm/llvm-project/issues/73623")
+    def test(self):
+        self.build()
+        self.dbg.CreateTarget(self.getBuildArtifact("a.out"))
+
+        self.expect_expr("alignof(Derived)", result_value="8")
