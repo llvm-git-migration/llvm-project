0f80a7ca467aa676ac0bfcbc4ea4a78db428b45c
Han-Kuan Chen <hankuan.chen@sifive.com>
2024-06-28 02:30:43 -0700

    [SLP][REVEC] Make getAltInstrMask support vector instructions.
    Fix "Mask and VecTy are incompatible" from
    
      LLVM :: Transforms/SLPVectorizer/X86/cmp_commute-inseltpoison.ll
      LLVM :: Transforms/SLPVectorizer/X86/cmp_commute.ll
      LLVM :: Transforms/SLPVectorizer/X86/splat-buildvector.ll

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -1032,10 +1032,13 @@ static void fixupOrderingIndices(MutableArrayRef<unsigned> Order) {
 /// Opcode1.
 SmallBitVector getAltInstrMask(ArrayRef<Value *> VL, unsigned Opcode0,
                                unsigned Opcode1) {
-  SmallBitVector OpcodeMask(VL.size(), false);
+  Type *ScalarTy = VL[0]->getType();
+  unsigned ScalarTyNumElements = getNumElements(ScalarTy);
+  SmallBitVector OpcodeMask(VL.size() * ScalarTyNumElements, false);
   for (unsigned Lane : seq<unsigned>(VL.size()))
     if (cast<Instruction>(VL[Lane])->getOpcode() == Opcode1)
-      OpcodeMask.set(Lane);
+      for (unsigned I = 0; I != ScalarTyNumElements; ++I)
+        OpcodeMask.set(Lane * ScalarTyNumElements + I);
   return OpcodeMask;
 }
 
