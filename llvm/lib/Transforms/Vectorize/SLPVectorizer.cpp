4585efadb1348532ad73f009e65dbbe045d9e7de
Han-Kuan Chen <hankuan.chen@sifive.com>
2024-06-28 02:41:19 -0700

    [SLP][REVEC] Make getGatherCost support vector instructions.
    Fix "Vector size mismatch" from
    
      LLVM :: Transforms/SLPVectorizer/X86/alternate-cmp-swapped-pred.ll
      LLVM :: Transforms/SLPVectorizer/X86/insert-element-build-vector-inseltpoison.ll
      LLVM :: Transforms/SLPVectorizer/X86/insert-element-build-vector.ll
      LLVM :: Transforms/SLPVectorizer/X86/reduction-logical.ll
      LLVM :: Transforms/SLPVectorizer/X86/reorder-clustered-node.ll

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -11358,7 +11358,8 @@ InstructionCost BoUpSLP::getGatherCost(ArrayRef<Value *> VL, bool ForPoisonSrc,
   // Find the cost of inserting/extracting values from the vector.
   // Check if the same elements are inserted several times and count them as
   // shuffle candidates.
-  APInt ShuffledElements = APInt::getZero(VL.size());
+  unsigned ScalarTyNumElements = getNumElements(ScalarTy);
+  APInt ShuffledElements = APInt::getZero(VecTy->getNumElements());
   DenseMap<Value *, unsigned> UniqueElements;
   constexpr TTI::TargetCostKind CostKind = TTI::TCK_RecipThroughput;
   InstructionCost Cost;
@@ -11378,7 +11379,8 @@ InstructionCost BoUpSLP::getGatherCost(ArrayRef<Value *> VL, bool ForPoisonSrc,
     Value *V = VL[I];
     // No need to shuffle duplicates for constants.
     if ((ForPoisonSrc && isConstant(V)) || isa<UndefValue>(V)) {
-      ShuffledElements.setBit(I);
+      for (unsigned J = 0; J != ScalarTyNumElements; ++J)
+        ShuffledElements.setBit(I * ScalarTyNumElements + J);
       ShuffleMask[I] = isa<PoisonValue>(V) ? PoisonMaskElem : I;
       continue;
     }
@@ -11391,7 +11393,8 @@ InstructionCost BoUpSLP::getGatherCost(ArrayRef<Value *> VL, bool ForPoisonSrc,
     }
 
     DuplicateNonConst = true;
-    ShuffledElements.setBit(I);
+    for (unsigned J = 0; J != ScalarTyNumElements; ++J)
+      ShuffledElements.setBit(I * ScalarTyNumElements + J);
     ShuffleMask[I] = Res.first->second;
   }
   if (ForPoisonSrc)
