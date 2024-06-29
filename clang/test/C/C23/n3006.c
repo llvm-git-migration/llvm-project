3b9b9e8cb48c460e6044737f73b1abc3e22b8590
Guillot Tony <tony.guillot@protonmail.com>
2024-06-29 19:27:23 +0200

    Added constexpr test cases

--- clang/test/C/C23/n3006.c
+++ clang/test/C/C23/n3006.c
@@ -9,6 +9,7 @@ union U1 { int a; double b; };  // expected-note {{previous definition is here}}
 enum E1 { FOO, BAR };           // expected-note {{previous definition is here}}
 
 auto normal_struct = (struct S1){ 1, 2 };
+auto normal_struct2 = (struct S1) { .x = 1, .y = 2 };
 auto underspecified_struct = (struct S2 { int x, y; }){ 1, 2 };           // expected-error {{'struct S2' is defined as an underspecified object initializer}}
 auto underspecified_struct_redef = (struct S1 { char x, y; }){ 'A', 'B'}; // expected-error {{redefinition of 'S1'}}
 auto underspecified_empty_struct = (struct S3 { }){ };                    // expected-error {{'struct S3' is defined as an underspecified object initializer}}
@@ -25,3 +26,31 @@ auto underspecified_enum = (enum E2 { BAZ, QUX }){ BAZ };       // expected-erro
 auto underspecified_enum_redef = (enum E1 { ONE, TWO }){ ONE }; // expected-error {{redefinition of 'E1'}}
 auto underspecified_empty_enum = (enum E3 {  }){ };             // expected-error {{'enum E3' is defined as an underspecified object initializer}} \
                                                                    expected-error {{use of empty enum}}
+
+// Constexpr tests
+constexpr auto ce_struct = (struct S1){ 1, 2 };
+constexpr auto ce_union = (union U1){ .a = 12 };
+constexpr auto ce_enum = (enum E1){ FOO };
+
+int func() {
+  struct S { int x, y; };
+  constexpr int i = (struct T { int a, b; }){0, 1}.a;
+
+  struct T t = { 1, 2 };
+}
+
+void func2() {
+  int x = (struct Foo { int x; }){ 0 }.x;
+}
+
+void func3() {
+  constexpr int x = (struct Foo { int x; }){ 0 }.x;
+}
+
+void test() {
+    constexpr typeof(struct s *) x = 0; // declares `s` which is not an ordinary identifier
+    constexpr struct S { int a, b; } y = { 0 }; // declares `S`, `a`, and `b`, none of which are ordinary identifiers
+    constexpr int a = 0, b = 0; // declares `a` and `b` as ordinary identifiers
+    auto c = (struct T { int x, y; }){0, 0}; // declares `T`, `x`, and `y`, none of which are ordinary identifiers
+    constexpr int (*fp)(struct X { int x; } val) = 0; // declares `X` and `x` which are not ordinary identifiers
+}
