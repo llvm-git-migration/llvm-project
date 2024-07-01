1f3d0498ccedc4c8adef01d44c29be8b1a78b133
Med Ismail Bennani <ismail@bennani.ma>
2024-07-01 11:53:49 -0700

    [lldb/Commands] Alias `script` command to `scripting execute`
    This patch introduces a new top-level `scripting` command with an
    `execute` sub-command, that basically replaces the `script` raw command.
    
    To avoid breaking the `script` command usages, this patch also adds an
    `script` alias to the `scripting execute` sub-command.
    
    The reason behind this change is to have a top-level command that will
    cover scripting related subcommands.
    
    Signed-off-by: Med Ismail Bennani <ismail@bennani.ma>

--- lldb/test/API/functionalities/abbreviation/TestAbbreviations.py
+++ lldb/test/API/functionalities/abbreviation/TestAbbreviations.py
@@ -80,7 +80,7 @@ class AbbreviationsTestCase(TestBase):
         # Check a command that wants the raw input.
         command_interpreter.ResolveCommand(r"""sc print("\n\n\tHello!\n")""", result)
         self.assertTrue(result.Succeeded())
-        self.assertEqual(r"""script print("\n\n\tHello!\n")""", result.GetOutput())
+        self.assertEqual(r"""scripting execute print("\n\n\tHello!\n")""", result.GetOutput())
 
         # Prompt changing stuff should be tested, but this doesn't seem like the
         # right test to do it in.  It has nothing to do with aliases or abbreviations.
