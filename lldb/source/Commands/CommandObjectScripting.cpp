8302f1d0bb80b688c82332644d727adb6f306fa2
Med Ismail Bennani <ismail@bennani.ma>
2024-07-01 02:54:33 -0700

    [lldb/Commands] Add `scripting template list` command with auto discovery
    This patch introduces a new `template` multiword sub-command to the
    `scripting` top-level command. As the name suggests, this sub-command
    operates on scripting templates, and currently has the ability to
    automatically discover the various scripting extensions that lldb
    supports.
    
    Signed-off-by: Med Ismail Bennani <ismail@bennani.ma>

--- lldb/source/Commands/CommandObjectScripting.cpp
+++ lldb/source/Commands/CommandObjectScripting.cpp
@@ -8,6 +8,7 @@
 
 #include "CommandObjectScripting.h"
 #include "lldb/Core/Debugger.h"
+#include "lldb/Core/PluginManager.h"
 #include "lldb/DataFormatters/DataVisualization.h"
 #include "lldb/Host/Config.h"
 #include "lldb/Host/OptionParser.h"
@@ -20,6 +21,8 @@
 
 using namespace lldb;
 using namespace lldb_private;
+
+#pragma mark CommandObjectScriptingExecute
 
 #define LLDB_OPTIONS_scripting_execute
 #include "CommandOptions.inc"
@@ -114,6 +117,159 @@ void CommandObjectScriptingExecute::DoExecute(llvm::StringRef command,
     result.SetStatus(eReturnStatusFailed);
 }
 
+#pragma mark CommandObjectScriptingTemplateList
+
+#define LLDB_OPTIONS_scripting_template_list
+#include "CommandOptions.inc"
+
+Status CommandObjectScriptingTemplateList::CommandOptions::SetOptionValue(
+    uint32_t option_idx, llvm::StringRef option_arg,
+    ExecutionContext *execution_context) {
+  Status error;
+  const int short_option = m_getopt_table[option_idx].val;
+
+  switch (short_option) {
+  case 'l':
+    language = (lldb::ScriptLanguage)OptionArgParser::ToOptionEnum(
+        option_arg, GetDefinitions()[option_idx].enum_values,
+        eScriptLanguageNone, error);
+    if (!error.Success())
+      error.SetErrorStringWithFormat("unrecognized value for language '%s'",
+                                     option_arg.str().c_str());
+    break;
+  default:
+    llvm_unreachable("Unimplemented option");
+  }
+
+  return error;
+}
+
+void CommandObjectScriptingTemplateList::CommandOptions::OptionParsingStarting(
+    ExecutionContext *execution_context) {
+  language = lldb::eScriptLanguageNone;
+}
+
+llvm::ArrayRef<OptionDefinition>
+CommandObjectScriptingTemplateList::CommandOptions::GetDefinitions() {
+  return llvm::ArrayRef(g_scripting_execute_options);
+}
+
+CommandObjectScriptingTemplateList::CommandObjectScriptingTemplateList(
+    CommandInterpreter &interpreter)
+    : CommandObjectParsed(
+          interpreter, "scripting template list",
+          "List all the available scripting affordances templates. ",
+          "scripting template list [--language <scripting-language> --]") {}
+
+CommandObjectScriptingTemplateList::~CommandObjectScriptingTemplateList() =
+    default;
+
+void CommandObjectScriptingTemplateList::DoExecute(
+    Args &command, CommandReturnObject &result) {
+  lldb::ScriptLanguage language =
+      (m_options.language == lldb::eScriptLanguageNone)
+          ? m_interpreter.GetDebugger().GetScriptLanguage()
+          : m_options.language;
+
+  if (language == lldb::eScriptLanguageNone) {
+    result.AppendError(
+        "the script-lang setting is set to none - scripting not available");
+    return;
+  }
+
+  ScriptInterpreter *script_interpreter =
+      GetDebugger().GetScriptInterpreter(true, language);
+
+  if (script_interpreter == nullptr) {
+    result.AppendError("no script interpreter");
+    return;
+  }
+
+  Stream &s = result.GetOutputStream();
+  s.Printf("Available scripted affordances:\n");
+
+  auto print_field = [&s](llvm::StringRef key, llvm::StringRef value,
+                          bool check_validy = false) {
+    if (!check_validy || !value.empty()) {
+      s.IndentMore();
+      s.Indent();
+      s << key << ": " << value << '\n';
+      s.IndentLess();
+    }
+  };
+  auto print_usages = [&s](llvm::StringRef usage_kind,
+                           std::vector<llvm::StringRef> &usages) {
+    s.IndentMore();
+    s.Indent();
+    s << usage_kind << " Usages:";
+    if (usages.empty())
+      s << " No usages.\n";
+    else if (usages.size() == 1)
+      s << " " << usages.front() << '\n';
+    else {
+      s << '\n';
+      for (llvm::StringRef usage : usages) {
+        s.IndentMore();
+        s.Indent();
+        s << usage << '\n';
+        s.IndentLess();
+      }
+    }
+    s.IndentLess();
+  };
+
+  size_t i = 0;
+  for (llvm::StringRef plugin_name =
+           PluginManager::GetScriptedInterfaceNameAtIndex(i);
+       !plugin_name.empty();) {
+
+    llvm::StringRef desc =
+        PluginManager::GetScriptedInterfaceDescriptionAtIndex(i);
+    lldb::ScriptLanguage lang =
+        PluginManager::GetScriptedInterfaceLanguageAtIndex(i);
+    std::vector<llvm::StringRef> ci_usages =
+        PluginManager::GetScriptedInterfaceCommandInterpreterUsagesAtIndex(i);
+    std::vector<llvm::StringRef> api_usages =
+        PluginManager::GetScriptedInterfaceAPIUsagesAtIndex(i);
+
+    print_field("Name", plugin_name);
+    switch (lang) {
+    case eScriptLanguagePython:
+      print_field("Language", "Python");
+      break;
+    case eScriptLanguageLua:
+      print_field("Language", "Lua");
+      break;
+    default:
+      break;
+    }
+    print_field("Description", desc);
+    print_usages("Command Interpreter", ci_usages);
+    print_usages("API", api_usages);
+
+    plugin_name = PluginManager::GetScriptedInterfaceNameAtIndex(++i);
+    if (!plugin_name.empty())
+      s.EOL();
+  }
+}
+
+#pragma mark CommandObjectMultiwordScriptingTemplate
+
+// CommandObjectMultiwordScriptingTemplate
+
+CommandObjectMultiwordScriptingTemplate::
+    CommandObjectMultiwordScriptingTemplate(CommandInterpreter &interpreter)
+    : CommandObjectMultiword(
+          interpreter, "scripting template",
+          "Commands for operating on the scripting templates.",
+          "scripting template [<subcommand-options>]") {
+  LoadSubCommand("list", CommandObjectSP(new CommandObjectScriptingTemplateList(
+                             interpreter)));
+}
+
+CommandObjectMultiwordScriptingTemplate::
+    ~CommandObjectMultiwordScriptingTemplate() = default;
+
 #pragma mark CommandObjectMultiwordScripting
 
 // CommandObjectMultiwordScripting
@@ -126,6 +282,9 @@ CommandObjectMultiwordScripting::CommandObjectMultiwordScripting(
           "scripting <subcommand> [<subcommand-options>]") {
   LoadSubCommand("execute", CommandObjectSP(new CommandObjectScriptingExecute(
                                 interpreter)));
+  LoadSubCommand("template",
+                 CommandObjectSP(
+                     new CommandObjectMultiwordScriptingTemplate(interpreter)));
 }
 
 CommandObjectMultiwordScripting::~CommandObjectMultiwordScripting() = default;
