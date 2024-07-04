25a8eff5fb790438f7d96e59bfdca22781e032e0
Alastair Houghton <ahoughton@apple.com>
2024-07-04 14:54:45 +0100

    [Parser][ObjC] Just add the last copy of each argument.
    We probably should just add the last copy of the `-Wobjc-prefix` argument(s).
    
    rdar://131055157

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -6243,8 +6243,8 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 
   Args.AddAllArgs(CmdArgs, options::OPT_Wsystem_headers_in_module_EQ);
 
-  Args.AddAllArgs(CmdArgs, options::OPT_Wobjc_prefixes_EQ);
-  Args.AddAllArgs(CmdArgs, options::OPT_Wobjc_forbidden_prefixes_EQ);
+  Args.AddLastArg(CmdArgs, options::OPT_Wobjc_prefixes_EQ);
+  Args.AddLastArg(CmdArgs, options::OPT_Wobjc_forbidden_prefixes_EQ);
   Args.AddLastArg(CmdArgs, options::OPT_Wobjc_prefix_length_EQ);
 
   if (Args.hasFlag(options::OPT_pedantic, options::OPT_no_pedantic, false))
