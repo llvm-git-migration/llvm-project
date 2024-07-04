6506043805237d077fa0bd3d548ead8d35c20d73
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2024-07-04 10:22:24 +0200

    [ValueTracking] Let ComputeKnownSignBits handle (shl (zext X), C)
    Add simple support for looking through a zext when doing
    ComputeKnownSignBits for shl. This is valid for the case when
    all extended bits are shifted out, because then the number of sign
    bits can be found by analysing the zext operand.
    
    The solution here is simple as it only handle a single zext (not
    passing remaining left shift amount during recursion). It could be
    possible to generalize this in the future by for example passing an
    'OffsetFromMSB' parameter to ComputeNumSignBitsImpl, telling it to
    calculate number of sign bits starting at some offset from the most
    significant bit.

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -3757,11 +3757,21 @@ static unsigned ComputeNumSignBitsImpl(const Value *V,
     }
     case Instruction::Shl: {
       const APInt *ShAmt;
+      Value *X = nullptr;
       if (match(U->getOperand(1), m_APInt(ShAmt))) {
         // shl destroys sign bits.
-        Tmp = ComputeNumSignBits(U->getOperand(0), Depth + 1, Q);
-        if (ShAmt->uge(TyBits) ||   // Bad shift.
-            ShAmt->uge(Tmp)) break; // Shifted all sign bits out.
+        if (ShAmt->uge(TyBits))
+          break; // Bad shift.
+        // We can look through a zext (more or less treating it as a sext) if
+        // all extended bits are shifted out.
+        if (match(U->getOperand(0), m_ZExt(m_Value(X))) &&
+            ShAmt->uge(TyBits - X->getType()->getScalarSizeInBits())) {
+          Tmp = ComputeNumSignBits(X, Depth + 1, Q);
+          Tmp += TyBits - X->getType()->getScalarSizeInBits();
+        } else
+          Tmp = ComputeNumSignBits(U->getOperand(0), Depth + 1, Q);
+        if (ShAmt->uge(Tmp))
+          break; // Shifted all sign bits out.
         Tmp2 = ShAmt->getZExtValue();
         return Tmp - Tmp2;
       }
