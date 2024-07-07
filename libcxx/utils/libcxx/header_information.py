8e30ce29e3c0985b5f3493339a6cd0961561284e
Mark de Wever <koraq@xs4all.nl>
2024-07-07 15:34:29 +0200

    [libc++] Update status after the St. Louis meeting.
    This updates:
    - The status tables
    - Feature test macros
    - New headers for modules

--- libcxx/utils/libcxx/header_information.py
+++ libcxx/utils/libcxx/header_information.py
@@ -139,10 +139,12 @@ mandatory_inclusions = {
 # modules will fail to build if a header is added but this list is not updated.
 headers_not_available = [
     "debugging",
+    "execution",
     "flat_map",
     "flat_set",
     "generator",
     "hazard_pointer",
+    "inplace_vector",
     "linalg",
     "rcu",
     "spanstream",
