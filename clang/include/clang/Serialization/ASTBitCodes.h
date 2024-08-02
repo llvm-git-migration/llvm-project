4da98aa5ad88276fc957571bfbebdd57a5f3918d
Caroline Concatto <caroline.concatto@arm.com>
2024-08-02 08:47:18 +0000

    [CLANG]Add Scalable vectors for mfloat8_t
    This patch adds these new vector sizes for sve:
    svmfloat8_t
    
    According to the ARM ACLE PR#323[1].
    
    [1] ARM-software/acle#323

--- clang/include/clang/Serialization/ASTBitCodes.h
+++ clang/include/clang/Serialization/ASTBitCodes.h
@@ -1109,7 +1109,7 @@ enum PredefinedTypeIDs {
 ///
 /// Type IDs for non-predefined types will start at
 /// NUM_PREDEF_TYPE_IDs.
-const unsigned NUM_PREDEF_TYPE_IDS = 503;
+const unsigned NUM_PREDEF_TYPE_IDS = 505;
 
 // Ensure we do not overrun the predefined types we reserved
 // in the enum PredefinedTypeIDs above.
