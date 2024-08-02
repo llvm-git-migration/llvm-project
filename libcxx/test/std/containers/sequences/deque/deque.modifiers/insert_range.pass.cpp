a8e6bc5e483480de52eec0137b07f2b2731a292e
Joseph Huber <huberjn@outlook.com>
2024-08-02 10:43:06 -0500

    [libcxx] Add testing configuration for GPU targets
    Summary:
    The GPU runs these tests using the files built from the `libc` project.
    These will be placed in `include/<triple>` and `lib/<triple>`. We use
    the `amdhsa-loader` and `nvptx-loader` tools, which are also provided by
    `libc`. These launch a kernel called `_start` which calls `main` so we
    can pretend like GPU programs are normal terminal applications.
    
    We force serial exeuction here, because `llvm-lit` runs way too many
    processes in parallel, which has a bad habit of making the GPU drivers
    hang or run out of resources. This allows the compilation to be run in
    parallel while the jobs themselves are serialized via a file lock.
    
    In the future this can likely be refined to accept user specified
    architectures, or better handle including the root directory by exposing
    that instead of just `include/<triple>/c++/v1/`.
    
    This currently fails ~1% of the tests on AMDGPU and ~3% of the tests on
    NVPTX. This will hopefully be reduced further, and later patches can
    XFAIL a lot of them once it's down to a reasonable number.
    
    Future support will likely want to allow passing in a custom
    architecture instead of simply relying on `-mcpu=native`.

--- libcxx/test/std/containers/sequences/deque/deque.modifiers/insert_range.pass.cpp
+++ libcxx/test/std/containers/sequences/deque/deque.modifiers/insert_range.pass.cpp
@@ -6,6 +6,10 @@
 //
 //===----------------------------------------------------------------------===//
 
+// FIXME: This takes over an hour to compile, disable for now.
+// UNSUPPORTED: target=amdgcn-amd-amdhsa
+// UNSUPPORTED: target=nvptx64-nvidia-cuda
+
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 // UNSUPPORTED: GCC-ALWAYS_INLINE-FIXME
 
