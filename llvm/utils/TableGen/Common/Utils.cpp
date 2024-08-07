efe5c33a1e29bf28772dc26a4fa24eb078509ad9
Rahul Joshi <rjoshi@nvidia.com>
2024-08-06 17:04:28 -0700

    [TableGen] Rework error reporting for duplicate Feature/Processor.
    - Extract code for sorting and checking duplicate Records into a helper
      function and update `collectProcModels` to use the helper.
    - Update `FeatureKeyValues` to:
      (a) Remove code for duplicate checks and use the helper.
      (b) Trim features with empty name explicitly to be able to use
          the helper.
    - Make the sorting deterministic by using record name as a secondary
      key for sorting, and re-enable SubtargetFeatureUniqueNames.td test
      that was disabled due to the non-determinism of the error messages.
    - Change wording of error message when duplicate records are found to
      be source code position agnostic, since `First` may not be before
      `Second` lexically.

---
+++ llvm/utils/TableGen/Common/Utils.cpp
@@ -0,0 +1,37 @@
+//===- Utils.cpp - Common Utilities -----------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Utils.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/TableGen/Error.h"
+#include "llvm/TableGen/Record.h"
+#include <algorithm>
+
+using namespace llvm;
+
+/// Sort an array of Records on the "Name" field, and check for records with
+/// duplicate "Name" field. If duplicates are found, report a fatal error.
+void llvm::sortAndReportDuplicates(MutableArrayRef<Record *> Records,
+                                   StringRef ObjectName) {
+  llvm::sort(Records, LessRecordFieldNameAndName());
+
+  auto I = std::adjacent_find(Records.begin(), Records.end(),
+                              [](const Record *Rec1, const Record *Rec2) {
+                                return Rec1->getValueAsString("Name") ==
+                                       Rec2->getValueAsString("Name");
+                              });
+  if (I == Records.end())
+    return;
+
+  // Found a duplicate name.
+  const Record *First = *I;
+  const Record *Second = *(I + 1);
+  StringRef Name = First->getValueAsString("Name");
+  PrintError(Second, ObjectName + " `" + Name + "` has multiple definitions.");
+  PrintFatalNote(First, "Another definition here.");
+}
