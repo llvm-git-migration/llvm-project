e1db193e4a4a54cd7ce191e24acd6fb74778c2ba
Michael Maitland <michaeltmaitland@gmail.com>
2024-08-08 11:16:32 -0700

    [LVL][CSA] Legalize CSA vectorization

--- llvm/include/llvm/Analysis/TargetTransformInfo.h
+++ llvm/include/llvm/Analysis/TargetTransformInfo.h
@@ -1800,6 +1800,10 @@ public:
         : EVLParamStrategy(EVLParamStrategy), OpStrategy(OpStrategy) {}
   };
 
+  /// \returns true if the loop vectorizer should vectorize conditional
+  /// scalar assignments for the target.
+  bool enableCSAVectorization() const;
+
   /// \returns How the target needs this vector-predicated operation to be
   /// transformed.
   VPLegalization getVPLegalizationStrategy(const VPIntrinsic &PI) const;
@@ -2225,6 +2229,7 @@ public:
                              SmallVectorImpl<Use *> &OpsToSink) const = 0;
 
   virtual bool isVectorShiftByScalarCheap(Type *Ty) const = 0;
+  virtual bool enableCSAVectorization() const = 0;
   virtual VPLegalization
   getVPLegalizationStrategy(const VPIntrinsic &PI) const = 0;
   virtual bool hasArmWideBranch(bool Thumb) const = 0;
@@ -3018,6 +3023,10 @@ public:
 
   bool isVectorShiftByScalarCheap(Type *Ty) const override {
     return Impl.isVectorShiftByScalarCheap(Ty);
+  }
+
+  bool enableCSAVectorization() const override {
+    return Impl.enableCSAVectorization();
   }
 
   VPLegalization
