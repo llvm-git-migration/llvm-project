a24983c4e848a0d1520c0fa25483bed76a7acbd0
Matt Arsenault <Matthew.Arsenault@amd.com>
2024-08-09 17:27:53 +0400

    NewPM/AMDGPU: Port AMDGPUPerfHintAnalysis to new pass manager
    This was much more difficult than I anticipated. The pass is
    not in a good state, with poor test coverage. The legacy PM
    does seem to be relying on maintaining the map state between
    different SCCs, which seems bad. The pass is going out of its
    way to avoid putting the attributes it introduces onto non-callee
    functions. If it just added them, we could use them directly
    instead of relying on the map, I would think.
    
    The NewPM path uses a ModulePass; I'm not sure if we should be
    using CGSCC here but there seems to be some missing infrastructure
    to support backend defined ones.

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -21,6 +21,7 @@
 #include "AMDGPUIGroupLP.h"
 #include "AMDGPUISelDAGToDAG.h"
 #include "AMDGPUMacroFusion.h"
+#include "AMDGPUPerfHintAnalysis.h"
 #include "AMDGPURegBankSelect.h"
 #include "AMDGPUSplitModule.h"
 #include "AMDGPUTargetObjectFile.h"
@@ -1229,7 +1230,7 @@ bool GCNPassConfig::addPreISel() {
   addPass(createLCSSAPass());
 
   if (TM->getOptLevel() > CodeGenOptLevel::Less)
-    addPass(&AMDGPUPerfHintAnalysisID);
+    addPass(&AMDGPUPerfHintAnalysisLegacyID);
 
   return false;
 }
