9f1a10c5837672e12fa297ecff5328f854acffc2
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-08-12 14:53:31 +0000

    [lldb-dap] Implement value locations for function pointers
    This commit adds `valueLocationReference` to function pointers and
    function references. Thereby, users can navigate directly to the
    pointed-to function from within the "variables" pane.
    
    In general, it would be useful to also a similar location references
    also to member function pointers, `std::source_location`,
    `std::function`,  and many more. Doing so would require extending the
    formatters to provide such a source code location.
    
    There were two RFCs about this a while ago:
    https://discourse.llvm.org/t/rfc-extending-formatters-with-a-source-code-reference/68375
    https://discourse.llvm.org/t/rfc-sbvalue-metadata-provider/68377/26
    
    However, both RFCs ended without a conclusion. As such, this commit now
    solve the lowest-hanging fruit, i.e. function pointers. If people find
    it useful, I will revive the RFC afterwards.

--- lldb/tools/lldb-dap/JSONUtils.cpp
+++ lldb/tools/lldb-dap/JSONUtils.cpp
@@ -1088,6 +1088,18 @@ std::string VariableDescription::GetResult(llvm::StringRef context) {
   return description.trim().str();
 }
 
+bool HasValueLocation(lldb::SBValue v) {
+  if (!v.GetType().IsPointerType() && !v.GetType().IsReferenceType()) {
+    return false;
+  }
+
+  lldb::addr_t addr = v.GetValueAsAddress();
+  lldb::SBLineEntry line_entry =
+      g_dap.target.ResolveLoadAddress(addr).GetLineEntry();
+
+  return line_entry.IsValid();
+}
+
 // "Variable": {
 //   "type": "object",
 //   "description": "A Variable is a name/value pair. Optionally a variable
@@ -1156,6 +1168,18 @@ std::string VariableDescription::GetResult(llvm::StringRef context) {
 //                       lifetime as the `variablesReference`. See 'Lifetime of
 //                       Object References' in the Overview section for
 //                       details."
+//     },
+//     "valueLocationReference": {
+//       "type": "integer",
+//       "description": "A reference that allows the client to request the
+//                       location where the variable's value is declared. For
+//                       example, if the variable contains a function pointer,
+//                       the adapter may be able to look up the function's
+//                       location. This should be present only if the adapter
+//                       is likely to be able to resolve the location.\n\nThis
+//                       reference shares the same lifetime as the
+//                       `variablesReference`. See 'Lifetime of Object
+//                       References' in the Overview section for details."
 //     },
 //
 //     "$__lldb_extensions": {
@@ -1270,7 +1294,10 @@ llvm::json::Value CreateVariable(lldb::SBValue v, int64_t var_ref,
     object.try_emplace("variablesReference", 0);
 
   if (v.GetDeclaration().IsValid())
-    object.try_emplace("declarationLocationReference", var_ref);
+    object.try_emplace("declarationLocationReference", var_ref << 1);
+
+  if (HasValueLocation(v))
+    object.try_emplace("valueLocationReference", (var_ref << 1) | 1);
 
   object.try_emplace("$__lldb_extensions", desc.GetVariableExtensionsJSON());
   return llvm::json::Value(std::move(object));
@@ -1293,8 +1320,8 @@ CreateRunInTerminalReverseRequest(const llvm::json::Object &launch_request,
                                   llvm::StringRef comm_file,
                                   lldb::pid_t debugger_pid) {
   llvm::json::Object run_in_terminal_args;
-  // This indicates the IDE to open an embedded terminal, instead of opening the
-  // terminal in a new window.
+  // This indicates the IDE to open an embedded terminal, instead of opening
+  // the terminal in a new window.
   run_in_terminal_args.try_emplace("kind", "integrated");
 
   auto launch_request_arguments = launch_request.getObject("arguments");
