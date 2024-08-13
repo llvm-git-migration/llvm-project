7a282dfb6c6b0df8608b854b7fb900d273be16d4
Tarun Prabhu <tarun.prabhu@gmail.com>
2024-08-12 18:45:25 -0600

    Fix tests. Check for failure on unsupported platforms

--- flang/test/Driver/frecord-command-line.f90
+++ flang/test/Driver/frecord-command-line.f90
@@ -3,9 +3,14 @@
 ! about anything. The correct lowering to a module attribute and beyond will
 ! be checked in other tests.
 !
-! RUN: %flang -### -frecord-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-RECORD %s
-! RUN: %flang -### -fno-record-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-NORECORD %s
+! RUN: %flang -### -target x86_64-unknown-linux -frecord-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-RECORD %s
+! RUN: %flang -### -target x86_64-unknown-macosx -frecord-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-RECORD %s
+! RUN: not %flang -### -target x86_64-unknown-windows -frecord-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-RECORD-ERROR %s
 
-! CHECK-RECORD: "-record-command-line" "{{.+}}/flang{{[^ ]*}} -### -frecord-command-line {{.+}}/frecord-command-line.f90 {{.*}}"
+! RUN: %flang -### -target x86_64-unknown-linux -fno-record-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-NO-RECORD %s
+! RUN: %flang -### -target x86_64-unknown-macosx -fno-record-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-NO-RECORD %s
+! RUN: %flang -### -target x86_64-unknown-windows -fno-record-command-line %s 2>&1 | FileCheck --check-prefix=CHECK-NO-RECORD %s
 
-! CHECK-NORECORD-NOT: "-record-command-line"
+! CHECK-RECORD: "-record-command-line" "{{.+}}/flang{{[^ ]*}} {{.+}} -frecord-command-line {{.+}}/frecord-command-line.f90 {{.*}}"
+! CHECK-NO-RECORD-NOT: "-record-command-line"
+! CHECK-RECORD-ERROR: error: unsupported option '-frecord-command-line' for target
