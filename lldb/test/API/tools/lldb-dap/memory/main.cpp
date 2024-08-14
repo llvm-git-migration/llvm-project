e01ea18961bbae0fb747b312670946bd768c5d73
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-08-14 11:52:40 +0000

    [lldb-dap] Support inspecting memory
    Adds support for the `readMemory` request which allows VS-Code to
    inspect memory. Also, add `memoryReference` to variablesa and `evaluate`
    responses, such that the binary view can be opened from the variables
    view and from the "watch" pane.

---
+++ lldb/test/API/tools/lldb-dap/memory/main.cpp
@@ -0,0 +1,10 @@
+#include <memory>
+#include <iostream>
+
+int main() {
+    int not_a_ptr = 666;
+    const char* rawptr = "dead";
+    std::unique_ptr<int> smartptr(new int(42));
+    // Breakpoint
+    return 0;
+}
