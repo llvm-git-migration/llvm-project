eaf0bd81002dc816ddbbd64e734aa11cd2ec49da
Sergey Kozub <skozub@nvidia.com>
2024-08-21 13:27:01 +0200

    [MLIR] Add f6E3M2FN type (#105573).
    `f6E3M2FN` type is proposed in [OpenCompute MX Specification](https://www.opencompute.org/documents/ocp-microscaling-formats-mx-v1-0-spec-final-pdf).
    It defines a 6-bit floating point number with bit layout S1E3M2. Unlike IEEE-754 types, there are no infinity or NaN values.
    
    ```c
    f6E3M2FN
    - Exponent bias: 3
    - Maximum stored exponent value: 7 (binary 111)
    - Maximum unbiased exponent value: 7 - 3 = 4
    - Minimum stored exponent value: 1 (binary 001)
    - Minimum unbiased exponent value: 1 − 3 = −2
    - Has Positive and Negative zero
    - Doesn't have infinity
    - Doesn't have NaNs
    
    Additional details:
    - Zeros (+/-): S.000.00
    - Max normal number: S.111.11 = ±2^(4) x (1 + 0.75) = ±28
    - Min normal number: S.001.00 = ±2^(-2) = ±0.25
    - Max subnormal number: S.000.11 = ±2^(-2) x 0.75 = ±0.1875
    - Min subnormal number: S.000.01 = ±2^(-2) x 0.25 = ±0.0625
    ```
    
    Related PRs:
    - [PR-94735](https://github.com/llvm/llvm-project/pull/94735) [APFloat] Add APFloat support for FP6 data types
    - [PR-97118](https://github.com/llvm/llvm-project/pull/97118) [MLIR] Add f8E4M3 type - was used as a template for this PR

--- mlir/include/mlir/IR/Builders.h
+++ mlir/include/mlir/IR/Builders.h
@@ -67,6 +67,7 @@ public:
   FloatType getFloat8E4M3FNUZType();
   FloatType getFloat8E4M3B11FNUZType();
   FloatType getFloat8E3M4Type();
+  FloatType getFloat6E3M2FNType();
   FloatType getBF16Type();
   FloatType getF16Type();
   FloatType getTF32Type();
