ac1b4f325d5952b971210a18933a896f940bc1aa
Krystian Stasiowski <sdkrystian@gmail.com>
2024-08-28 10:51:24 -0400

    [FOLD] fix common pointer being overwritten when deserializing

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -9951,7 +9951,7 @@ void ASTReader::finishPendingActions() {
 
     auto RTD = cast<RedeclarableTemplateDecl>(D)->getCanonicalDecl();
     for (auto *R = getMostRecentExistingDecl(RTD); R; R = R->getPreviousDecl())
-      cast<RedeclarableTemplateDecl>(R)->Common = RTD->Common;
+      cast<RedeclarableTemplateDecl>(R)->Common.setPointer(RTD->Common.getPointer());
   }
   PendingDefinitions.clear();
 
