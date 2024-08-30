51a9d45c379840f4778d71e59dacbdc76b2f959b
Henrik G. Olsson <h_olsson@apple.com>
2024-08-29 17:11:45 -0700

    [Utils] Add UTC support for lit's --update-tests
    Adds support for invoking the appropriate update_*_test_checks.py script
    from lit. Checks the header comment for which script was used to
    generate it in the first place, so only test cases that were already
    generated are affected.
    
    To support this the interface for test updater functions is expanded to
    not only take a ShellCommandResult, but also the Test object. This makes
    it easy to get the file path of the current test.
    
    Also adds a --path flag to update_any_test_checks.py as a convenience
    to avoid having to manually set the PATH variable.

--- clang/utils/UpdateVerifyTests/litplugin.py
+++ clang/utils/UpdateVerifyTests/litplugin.py
@@ -23,8 +23,8 @@ def get_verify_prefixes(command):
         prefixes.add(prefix)
     return prefixes
 
-    
-def verify_test_updater(result):
+
+def verify_test_updater(result, test):
     if not result.stderr:
         return None
     prefixes = get_verify_prefixes(result.command)
