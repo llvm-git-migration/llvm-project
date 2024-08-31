479911481cfcc33d6a550c37dbe057e858643191
Hristo Hristov <hghristov.rmm@gmail.com>
2024-08-31 23:18:20 +0300

    Cleanup + FTM

--- libcxx/test/std/utilities/meta/meta.unary/meta.unary.prop/is_implicit_lifetime.verify.cpp
+++ libcxx/test/std/utilities/meta/meta.unary/meta.unary.prop/is_implicit_lifetime.verify.cpp
@@ -8,15 +8,14 @@
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17, c++20
 
+// These compilers don't support __builtin_is_implicit_lifetime yet.
+// UNSUPPORTED: clang-17, clang-18, clang-19, gcc-14, apple-clang-16, apple-clang-17
+
 // <type_traits>
 
 // template<class T> struct is_implicit_lifetime;
 
-#include <cassert>
-#include <cstddef>
 #include <type_traits>
-
-#include "test_macros.h"
 
 struct IncompleteStruct;
 
@@ -25,13 +24,3 @@ static_assert(!std::is_implicit_lifetime<IncompleteStruct>::value);
 
 // expected-error@*:* {{atomic types are not supported in '__builtin_is_implicit_lifetime'}}
 static_assert(!std::is_implicit_lifetime<_Atomic int>::value);
-
-#if 0
-// FIXME: "variable length arrays in C++ are a Clang extension"
-void test(int n) {
-  int varArr[n];
-  using VarArrT = decltype(varArr);
-  // expected-error@*:* {{variable length arrays are not supported in '__builtin_is_implicit_lifetime'}}
-  static_assert(std::is_implicit_lifetime<VarArrT>::value);
-}
-#endif
