696b77b7c85f79fddb2b88f04c78933acd769d12
Timm Bäder <tbaeder@redhat.com>
2024-09-04 14:32:32 +0200

    [clang][bytecode] Allow continuing when discarded MemberExpr Base fails
    We don't need the value in this case, since we're discarding it anyway.
    Allow continuing the interpretation but note the side effect.

--- clang/test/SemaCXX/static-assert-cxx17.cpp
+++ clang/test/SemaCXX/static-assert-cxx17.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++1z -triple=x86_64-linux-gnu
+// RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++1z -triple=x86_64-linux-gnu -fexperimental-new-constant-interpreter
 
 template <typename U, typename V>
 struct S1 {
