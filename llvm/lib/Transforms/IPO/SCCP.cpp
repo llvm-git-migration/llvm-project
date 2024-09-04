d764613a2e99207ba8017f1cd15ab5640a3da843
hanbeom <kese111@gmail.com>
2024-09-04 19:33:42 +0900

    [SCCP] Remove LoadInst if it loaded from Constant GlobalVariable
    This patch removes the `LoadInst` when it loaded from Constant
    GlobalVariable. This allows `canRemoveInstruction` function to be
    removed.

--- llvm/lib/Transforms/IPO/SCCP.cpp
+++ llvm/lib/Transforms/IPO/SCCP.cpp
@@ -356,9 +356,13 @@ static bool runIPSCCP(
       continue;
     LLVM_DEBUG(dbgs() << "Found that GV '" << GV->getName()
                       << "' is constant!\n");
-    while (!GV->use_empty()) {
-      StoreInst *SI = cast<StoreInst>(GV->user_back());
-      SI->eraseFromParent();
+    for (User *U : make_early_inc_range(GV->users())) {
+      // We can remove LoadInst at here, because we already replace user of this
+      // to constant.
+      assert((isa<StoreInst>(U) || isa<LoadInst>(U)) &&
+             "Only Store|Load Instruction can be user of GlobalVariable at "
+             "reaching here.");
+      cast<Instruction>(U)->eraseFromParent();
     }
 
     // Try to create a debug constant expression for the global variable
