9cd55ecefe20d295975b638a0075f87991ca75a3
Matthias Springer <mspringer@nvidia.com>
2024-09-06 09:53:57 +0200

    [mlir][Transforms][NFC] Dialect conversion: Fix typo and improve docs

--- mlir/include/mlir/Transforms/DialectConversion.h
+++ mlir/include/mlir/Transforms/DialectConversion.h
@@ -1130,10 +1130,9 @@ struct ConversionConfig {
   /// builtin.unrealized_conversion_cast ops. The conversion process fails if
   /// at least one materialization could not be built.
   ///
-  /// If set to "false", the dialect conversion does not does not build any
-  /// custom materializations and instead inserts
-  /// builtin.unrealized_conversion_cast ops to ensure that the resulting IR
-  /// is valid.
+  /// If set to "false", the dialect conversion does not build any custom
+  /// materializations and instead inserts builtin.unrealized_conversion_cast
+  /// ops to ensure that the resulting IR is valid.
   bool buildMaterializations = true;
 };
 
