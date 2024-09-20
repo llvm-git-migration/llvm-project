2b2a5cbd178a359aac57e511bf02bb21950c3c6b
vfdev-5 <vfdev.5@gmail.com>
2024-09-20 22:30:49 +0200

    [skip-ci] Added lock on PyGlobals::get and PyMlirContext liveContexts WIP on adding multithreaded_tests

--- mlir/test/python/lib/PythonTestModulePybind11.cpp
+++ mlir/test/python/lib/PythonTestModulePybind11.cpp
@@ -23,7 +23,7 @@ static bool mlirTypeIsARankedIntegerTensor(MlirType t) {
          mlirTypeIsAInteger(mlirShapedTypeGetElementType(t));
 }
 
-PYBIND11_MODULE(_mlirPythonTestPybind11, m) {
+PYBIND11_MODULE(_mlirPythonTest, m, py::mod_gil_not_used()) {
   m.def(
       "register_python_test_dialect",
       [](MlirContext context, bool load) {
