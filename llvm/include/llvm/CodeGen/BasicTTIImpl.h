03e622e19b5e9eba7f9c12195141f3fffe5a047c
Benjamin Maxwell <benjamin.maxwell@arm.com>
2024-09-24 16:01:49 +0000

    [IR] Add `llvm.sincos` intrinsic
    This adds the `llvm.sincos` intrinsic, legalization, and lowering.
    
    The `llvm.sincos` intrinsic takes a floating-point value and returns
    both the sine and cosine (as a struct).
    
    ```
    declare { float, float }          @llvm.sincos.f32(float  %Val)
    declare { double, double }        @llvm.sincos.f64(double %Val)
    declare { x86_fp80, x86_fp80 }    @llvm.sincos.f80(x86_fp80  %Val)
    declare { fp128, fp128 }          @llvm.sincos.f128(fp128 %Val)
    declare { ppc_fp128, ppc_fp128 }  @llvm.sincos.ppcf128(ppc_fp128  %Val)
    declare { <4 x float>, <4 x float> } @llvm.sincos.v4f32(<4 x float>  %Val)
    ```
    
    The lowering is built on top of the existing FSINCOS ISD node, with
    additional type legalization to allow for f16, f128, and vector values.

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -1978,6 +1978,9 @@ public:
     case Intrinsic::cos:
       ISD = ISD::FCOS;
       break;
+    case Intrinsic::sincos:
+      ISD = ISD::FSINCOS;
+      break;
     case Intrinsic::tan:
       ISD = ISD::FTAN;
       break;
