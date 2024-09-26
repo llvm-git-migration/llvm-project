60c09775bdd770ebf516114a322df4ba6cbd2166
einvbri <vince.a.bridgers@ericsson.com>
2024-09-26 16:24:59 +0200

    [clang-tidy] [analyzer] Nondeterministic pointer usage improvements
    RFC: This PR is not ready to merge yet, preliminary comments are welcome.
         Here are some questions to consider...
    
    Q) Is the name and file location ok in the directory structure?
       I initially chose bugprone and NondeterministicPointerUsage as a
       starting point for review.
    Q) I wrote an explanation for the check based on internal discussion
       since I felt like this was missing. Please check and comment.
    Q) There are more ways to iterate over an unordered container
       than captured here. Do those need to be detected as well?
    Q) I squashed PointerIteration and PointerSorting. I think that works
       in this case, but interested to hear your comments about that.
    Q) I ended up expanding upon the C++ simulation header, but had thoughts
       about breaking that up into multiple smaller files. Maybe there's an
       opportunity to refactor some C++ simulation files across multiple
       checkers as a seperate PR first, or maybe even as part of this one?
    
    This change moves the alpha.nondeterministic.PointerSorting and
    alpha.nondeterministic.PointerIteration static analyzer checkers to
    a single clang-tidy check. Those checkers were implemented as clang-tidy
    checks wrapped in the static analyzer framework. The documentation was
    updated to describe what the checks can and cannot do, and testing
    was completed on a broad set of open source projects.

---
+++ clang-tools-extra/clang-tidy/bugprone/NondeterministicPointerUsageCheck.h
@@ -0,0 +1,36 @@
+//===--- NondeterministicPointerUsageCheck.h - clang-tidy ---*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE__NONDETERMINISTICPOINTERUSAGECHECK_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE__NONDETERMINISTICPOINTERUSAGECHECK_H
+
+#include "../ClangTidyCheck.h"
+
+namespace clang::tidy::bugprone {
+
+/// Finds temporaries that look like RAII objects.
+///
+/// For the user-facing documentation see:
+/// http://clang.llvm.org/extra/clang-tidy/checks/bugprone/nondeterministic-pointer-usage.html
+class NondeterministicPointerUsageCheck : public ClangTidyCheck {
+public:
+  NondeterministicPointerUsageCheck(StringRef Name, ClangTidyContext *Context)
+      : ClangTidyCheck(Name, Context) {}
+  bool isLanguageVersionSupported(const LangOptions &LangOpts) const override {
+    return LangOpts.CPlusPlus;
+  }
+  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+  std::optional<TraversalKind> getCheckTraversalKind() const override {
+    return TK_IgnoreUnlessSpelledInSource;
+  }
+};
+
+} // namespace clang::tidy::bugprone
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_BUGPRONE__NONDETERMINISTICPOINTERUSAGECHECK_H
