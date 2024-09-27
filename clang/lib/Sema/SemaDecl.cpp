84946607fc58927b1b44da2b70566abca041bbfb
David Spickett <david.spickett@linaro.org>
2024-09-27 10:19:19 +0000

    [clang][Sema] Bad register variable type error should point to the type
    ...not the register keyword. Fixes #109776.
    
    Until now the error was only tested in clang/test/Sema/asm.c,
    where you can't check for the "^" character. So I've added a new
    caret test file as I see has been done for other error types.

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -7949,7 +7949,8 @@ NamedDecl *Sema::ActOnVariableDeclarator(
       }
 
       if (!R->isIntegralType(Context) && !R->isPointerType()) {
-        Diag(D.getBeginLoc(), diag::err_asm_bad_register_type);
+        Diag(TInfo->getTypeLoc().getBeginLoc(),
+             diag::err_asm_bad_register_type);
         NewVD->setInvalidDecl(true);
       }
     }
