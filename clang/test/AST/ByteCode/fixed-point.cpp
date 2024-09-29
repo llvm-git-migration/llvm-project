20b0c417f53bc3c2094ee2f37c0aae150d7a1932
Timm Bäder <tbaeder@redhat.com>
2024-09-29 10:53:44 +0200

    [clang][bytecode] Implement fixed-point-to-int casts
    And some cleanups around overflow handling.

--- clang/test/AST/ByteCode/fixed-point.cpp
+++ clang/test/AST/ByteCode/fixed-point.cpp
@@ -26,6 +26,11 @@ namespace IntToFixedPointCast {
   static_assert(sf == -1);
 }
 
+namespace FixedPointToIntCasts {
+  constexpr int I = A;
+  static_assert(I == -13);
+}
+
 namespace FloatToFixedPointCast {
   constexpr _Fract sf = 1.0; // both-error {{must be initialized by a constant expression}} \
                              // both-note {{outside the range of representable values of type 'const _Fract'}}
