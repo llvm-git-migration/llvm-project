b9dc55f8226baf5a6e00a5b0a7f84bb1a9ef0fa8
Kaviya Rajendiran <Kaviya.Rajendran@amd.com>
2024-10-04 19:14:20 +0530

    Support for lowering task_reduction and in_reduction to MLIR

--- flang/lib/Lower/OpenMP/ReductionProcessor.h
+++ flang/lib/Lower/OpenMP/ReductionProcessor.h
@@ -120,9 +120,10 @@ public:
 
   /// Creates a reduction declaration and associates it with an OpenMP block
   /// directive.
+  template <class T>
   static void addDeclareReduction(
       mlir::Location currentLocation, lower::AbstractConverter &converter,
-      const omp::clause::Reduction &reduction,
+      const T &reduction,
       llvm::SmallVectorImpl<mlir::Value> &reductionVars,
       llvm::SmallVectorImpl<bool> &reduceVarByRef,
       llvm::SmallVectorImpl<mlir::Attribute> &reductionDeclSymbols,
