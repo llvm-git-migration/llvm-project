afdaba612d84092bd7eed9d3161ba297094ef9e6
Keith Packard <keithp@keithp.com>
2024-10-04 21:08:17 -0700

    [compiler-rt] Support aarch64 targets without FPU
    Fall back to the old C implementation of __arm_sc_memset when
    the target doesn't have an FPU.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- compiler-rt/lib/builtins/aarch64/sme-libc-routines.c
+++ compiler-rt/lib/builtins/aarch64/sme-libc-routines.c
@@ -1,5 +1,20 @@
 #include <stddef.h>
 
+#if __ARM_FP == 0
+// WARNING: When building the scalar versions of these functions you need to
+// use the compiler flag "-mllvm -disable-loop-idiom-all" to prevent clang
+// from recognising a loop idiom and planting calls to memcpy!
+
+void *__arm_sc_memset(void *dest, int c, size_t n) __arm_streaming_compatible {
+  unsigned char *destp = (unsigned char *)dest;
+  unsigned char c8 = (unsigned char)c;
+  for (size_t i = 0; i < n; ++i)
+    destp[i] = c8;
+
+  return dest;
+}
+#endif
+
 const void *__arm_sc_memchr(const void *src, int c,
                             size_t n) __arm_streaming_compatible {
   const unsigned char *srcp = (const unsigned char *)src;
