5e5cb1673cd320959a058f5ba6a5059a27a74f83
liusy58 <liusy58@linux.alibaba.com>
2024-10-29 16:09:31 +0800

    [BOLT] Fix bug in `lookupStubFromGroup`
    The current implementation of `lookupStubFromGroup` is incorrect. The function
    is intended to find and return the closest stub using `lower_bound`, which
    identifies the first element in a sorted range that is not less than a specified
    value. However, if such an element is not found within `Candidates` and the list
    is not empty, the function returns `nullptr`. Instead, it should check whether
    the last element satisfies the condition.

---
+++ bolt/test/AArch64/one-stub.s
@@ -0,0 +1,28 @@
+# REQUIRES: system-linux, asserts
+# RUN: llvm-mc -filetype=obj -triple aarch64-unknown-unknown %s -o %t.o
+# RUN: %clang %cflags -O0 -fPIC -pie %t.o -o %t.exe -nostdlib -Wl,-q
+# RUN: link_fdata %s %t.o %t.fdata
+# RUN: llvm-bolt %t.exe -o %t.bolt  \
+# RUN:   --data %t.fdata  | FileCheck %s
+# CHECK: BOLT-INFO: Inserted 1 stubs in the hot area and 0 stubs in the cold area. 
+
+.section .text
+.global _start
+.global func
+
+  .align 4
+  .global _start
+  .type _start, %function
+_start:
+# FDATA: 0 [unknown] 0 1 _start 0 0 100
+    bl func
+    bl func
+    ret  
+  .space 0x8000000
+  .global func
+  .type func, %function
+func:
+    add x0, x0, #1
+    ret
+
+.reloc 0, R_AARCH64_NONE
