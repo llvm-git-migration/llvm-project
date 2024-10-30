e8fe0b3e63cdf9a2e16f680d59705074da10338e
Corentin Jabot <corentinjabot@gmail.com>
2024-10-30 10:25:42 +0100

    [Clang] Correctly initialize placeholder fields from their initializers
    We made the incorrect assumption that names of fields are unique
    when creating their default initializers.
    
    We fix that by keeping track of the instantiaation pattern
    for field decls that are placeholder vars,
    like we already do for unamed fields.
    
    Fixes #114069

--- clang/include/clang/AST/ASTContext.h
+++ clang/include/clang/AST/ASTContext.h
@@ -1037,7 +1037,7 @@ public:
   void setInstantiatedFromUsingShadowDecl(UsingShadowDecl *Inst,
                                           UsingShadowDecl *Pattern);
 
-  FieldDecl *getInstantiatedFromUnnamedFieldDecl(FieldDecl *Field);
+  FieldDecl *getInstantiatedFromUnnamedFieldDecl(FieldDecl *Field) const;
 
   void setInstantiatedFromUnnamedFieldDecl(FieldDecl *Inst, FieldDecl *Tmpl);
 
