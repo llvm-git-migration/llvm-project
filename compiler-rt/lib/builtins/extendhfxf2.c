a1096bd7eb8478555cc78b9339964d3f000171f0
B I Mohammed Abbas <bimohammadabbas@gmail.com>
2024-10-30 14:49:58 +0530

    Add extendhfxf2 to compiler-rt for x86
    Co-authored-by: Alexander Richardson <mail@alexrichardson.me>

---
+++ compiler-rt/lib/builtins/extendhfxf2.c
@@ -0,0 +1,21 @@
+//===-- lib/extendhfxf2.c - half -> long double conversion --------*- C -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "int_lib.h"
+#define SRC_HALF
+#define DST_DOUBLE
+#if HAS_80_BIT_LONG_DOUBLE
+#include "fp_extend_impl.inc"
+
+// Use a forwarding definition and noinline to implement a poor man's alias,
+// as there isn't a good cross-platform way of defining one.
+// Long double are expected to be as precise as double.
+COMPILER_RT_ABI long double __extendhfxf2(src_t a) {
+  return __extendXfYf2__(a);
+}
+#endif
