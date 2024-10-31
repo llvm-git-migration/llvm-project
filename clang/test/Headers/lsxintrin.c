1bbbc1c38715f42e6014ee319d76af9a4015e4d5
Xi Ruoyao <xry111@xry111.site>
2024-11-01 00:00:14 +0800

    [LoongArch][Clang] Add tests for #110834

---
+++ clang/test/Headers/lsxintrin.c
@@ -0,0 +1,6 @@
+// REQUIRES: loongarch-registered-target
+// RUN: %clang_cc1 %s -fsyntax-only -triple loongarch64-linux -target-feature +lsx
+// RUN: %clang_cc1 %s -fsyntax-only -triple loongarch64-linux -target-feature +lsx -flax-vector-conversions=none
+// RUN: %clang_cc1 %s -fsyntax-only -triple loongarch64-linux -target-feature +lsx -flax-vector-conversions=none -fno-signed-char
+
+#include <lsxintrin.h>
