af8b90ae3043d4e03720354cbb715a7a64faaf24
WANG Xuerui <git@xen0n.name>
2024-10-31 20:06:19 +0800

    [LoongArch] Record the special AMO operand constraint with TableGen
    The LoongArch Reference Manual says that the 3-register atomic memory
    operations cannot have their rd equal to either rj or rk if not zero
    [^1], and both GNU as and LLVM IAS enforce the constraint. However,
    currently LoongArch AsmParser is checking for the opcode with a direct
    numerical comparison on the opcode, which is enum-typed: the fact that
    all AMO insns have adjacent numerical values is merely a coincidence,
    and it is better to not rely on the current TableGen file layout and
    the particular TableGen implementation behavior.
    
    Instead, start to leverage the target-specific flags field of
    MCInstrDesc, and record the constraint with TableGen, so we can stop
    treating the opcode enum as number.
    
    While documenting the new flag, it was found that v1.10 of the Manual
    did not specify the similar constraint for the AMCAS instructions.
    Experiments were done on a Loongson 3A6000 (LA664 uarch) and it turned
    out that at least AMCAS will still signal INE with `rd == rj`. The
    `rd == rk` case should be a no-op according to the semantics, but as it
    is meaningless to perform CAS with the "old value" same as the "new
    value", it is not worth special-casing. So the current behavior of also
    enforcing the constraint for AMCAS is kept.
    
    [^1]: if `rd == rj` an INE would be signaled; if `rd == rk` it is UB.

--- llvm/lib/Target/LoongArch/AsmParser/LoongArchAsmParser.cpp
+++ llvm/lib/Target/LoongArch/AsmParser/LoongArchAsmParser.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "MCTargetDesc/LoongArchBaseInfo.h"
 #include "MCTargetDesc/LoongArchInstPrinter.h"
 #include "MCTargetDesc/LoongArchMCExpr.h"
 #include "MCTargetDesc/LoongArchMCTargetDesc.h"
@@ -1560,9 +1561,10 @@ bool LoongArchAsmParser::processInstruction(MCInst &Inst, SMLoc IDLoc,
 
 unsigned LoongArchAsmParser::checkTargetMatchPredicate(MCInst &Inst) {
   unsigned Opc = Inst.getOpcode();
+  const MCInstrDesc &MCID = MII.get(Opc);
   switch (Opc) {
   default:
-    if (Opc >= LoongArch::AMADD_D && Opc <= LoongArch::AMXOR_W) {
+    if (LoongArchII::isSubjectToAMORdConstraint(MCID.TSFlags)) {
       MCRegister Rd = Inst.getOperand(0).getReg();
       MCRegister Rk = Inst.getOperand(1).getReg();
       MCRegister Rj = Inst.getOperand(2).getReg();
