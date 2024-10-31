c6b5c084c894c97cc5003e0d2354e88cb7479bc9
Timm Bäder <tbaeder@redhat.com>
2024-10-31 16:00:31 +0100

    [clang][bytecode] Add more checks to _ai32_* builtins
    They are called in a few different forms that we don't support.

--- clang/test/CodeGen/builtins-hexagon.c
+++ clang/test/CodeGen/builtins-hexagon.c
@@ -1,5 +1,6 @@
 // REQUIRES: hexagon-registered-target
 // RUN: %clang_cc1 -triple hexagon-unknown-elf -target-cpu hexagonv65 -target-feature +hvxv65 -target-feature +hvx-length128b -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple hexagon-unknown-elf -target-cpu hexagonv65 -target-feature +hvxv65 -target-feature +hvx-length128b -emit-llvm %s -o - | FileCheck %s -fexperimental-new-constant-interpreter
 
 void test() {
   int v64 __attribute__((__vector_size__(64)));
