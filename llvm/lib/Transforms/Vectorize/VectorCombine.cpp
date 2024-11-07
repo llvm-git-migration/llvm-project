93364657c6660e16be0eda70fc5a36f282ad76cc
hanbeom <kese111@gmail.com>
2024-11-08 00:48:04 +0900

    use SmallVector with initial value

--- llvm/lib/Transforms/Vectorize/VectorCombine.cpp
+++ llvm/lib/Transforms/Vectorize/VectorCombine.cpp
@@ -2698,7 +2698,7 @@ bool VectorCombine::foldInsExtVectorToShuffle(Instruction &I) {
   if (ExtIdx >= NumElts)
     return false;
 
-  SmallVector<int> Mask(NumElts);
+  SmallVector<int> Mask(NumElts, 0);
   std::iota(Mask.begin(), Mask.end(), 0);
   Mask[InsIdx] = ExtIdx + NumElts;
   // Cost
