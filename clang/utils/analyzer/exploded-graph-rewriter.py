7faee31bc4bc0b1a9fd037a99f54856c84affc91
Balazs Benics <benicsbalazs@gmail.com>
2024-11-13 12:55:06 +0100

    [analyzer] Print the PostInitializer target in exploded-graph-rewriter
    This aids debugging PostInitializer program points by knowing what is
    the location being initialized.

--- clang/utils/analyzer/exploded-graph-rewriter.py
+++ clang/utils/analyzer/exploded-graph-rewriter.py
@@ -88,6 +88,8 @@ class ProgramPoint:
             )
         elif self.kind == "BlockEntrance":
             self.block_id = json_pp["block_id"]
+        elif self.kind == "PostInitializer":
+            self.target = json_pp["field_decl"] if "field_decl" in json_pp else json_pp["type"]
 
 
 # A single expression acting as a key in a deserialized Environment.
@@ -617,6 +619,13 @@ class DotDumpVisitor:
                 '<td align="left" width="0">'
                 '<font color="%s">%s</font></td>'
                 '<td align="left">[B%d]</td></tr>' % (color, p.kind, p.block_id)
+            )
+        elif p.kind == "PostInitializer":
+            self._dump(
+                '<td width="0"></td>'
+                '<td align="left" width="0">'
+                '<font color="%s">%s</font></td>'
+                '<td align="left">%s</td></tr>' % (color, p.kind, p.target)
             )
         else:
             # TODO: Print more stuff for other kinds of points.
