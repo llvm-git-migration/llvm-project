llvm-git-migration/llvm-project

commit cb90d5b3ef463f0a471f9c6d39978c3764021dea

[CMake] Enable CMP0156 if available (#115046)
Some linkers do not require that libraries are repeated on the command
line. The Apple linker emits warnings when duplicate libraries are
specified, resulting in a wall of warnings.

CMP0156 deduplicates libraries on the command line when the linker
doesn't require them.

This patch enables CMP0156 to quiet the warnings when using a version of
CMake that recognizes it (CMake 3.29 and newer).

cmake/Modules/CMakePolicy.cmake
@@ -30,3 +30,11 @@ endif()
 if(POLICY CMP0147)
   cmake_policy(SET CMP0147 NEW)
 endif()
+
+# CMP0156: De-duplicate libraries on link lines based on linker capabilities.
+# New in CMake 3.29: https://cmake.org/cmake/help/latest/policy/CMP0156.html
+# Avoids the deluge of 'ld: warning: ignoring duplicate libraries' warnings when
+# building with the Apple linker.
+if(POLICY CMP0156)
+  cmake_policy(SET CMP0156 NEW)
+endif()

commit dc704ac6932a5fadde1921585ffb611b01b8c39b

[CMake] Enable CMP0179 when enabling CMP0156 to avoid LLD static library link order bug
This fixes the following linking failure for libclang-cpp.so with -DLLVM_LINK_LLVM_DYLIB=ON:
```
ld.lld: error: undefined symbol: llvm::omp::getOpenMPClauseName(llvm::omp::Clause)
>>> referenced by OpenMPKinds.cpp
>>>               tools/clang/lib/Basic/CMakeFiles/obj.clangBasic.dir/OpenMPKinds.cpp.o:(clang::getOpenMPSimpleClauseTypeName(llvm::omp::Clause, unsigned int))
>>> referenced by SemaOpenMP.cpp
>>>               tools/clang/lib/Sema/CMakeFiles/obj.clangSema.dir/SemaOpenMP.cpp.o:(clang::SemaOpenMP::CheckOMPRequiresDecl(clang::SourceLocation, llvm::ArrayRef<clang::OMPClause*>))
>>> referenced by SemaOpenMP.cpp
>>>               tools/clang/lib/Sema/CMakeFiles/obj.clangSema.dir/SemaOpenMP.cpp.o:(clang::SemaOpenMP::CheckOMPRequiresDecl(clang::SourceLocation, llvm::ArrayRef<clang::OMPClause*>))
>>> referenced 166 more times

[tons more]
```

This is exhibited under the following conditions:
* LLVM LLD
* CMake 3.31.0
* CMP0156=NEW
* CMP0179=OLD

CMake 3.31's new linker architecture changes the library link order.
Per LLD's design documents it should be immune to such reodering like other popular linkers (gold, Apple's), but isn't.

A workaround for LLD is expected in CMake 3.31.1 but CMP0179=NEW changes the linking order such that the bug is avoided.
Deduplication is better in general so always enable it when possible.

See: https://gitlab.kitware.com/cmake/cmake/-/issues/26447
Fixes: cb90d5b3ef463f0a471f9c6d39978c3764021dea

cmake/Modules/CMakePolicy.cmake
@@ -37,4 +37,13 @@ endif()
 # building with the Apple linker.
 if(POLICY CMP0156)
   cmake_policy(SET CMP0156 NEW)
+
+  # CMP0179: De-duplication of static libraries on link lines keeps first occurrence.
+  # Dependent on CMP0156=NEW. Keeps the first occurrence of static libraries
+  # during deduplication to unify the behaviour on all platforms.
+  # Also works around LLD bug exhibited since CMake 3.31.0 with an expected
+  # workaround in 3.31.1 (https://gitlab.kitware.com/cmake/cmake/-/issues/26447).
+  if(POLICY CMP0179)
+    cmake_policy(SET CMP0179 NEW)
+  endif()
 endif()