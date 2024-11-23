7e87ea165e5586a63b1a1ea09a206b7d7727434a
Craig Topper <craig.topper@sifive.com>
2024-11-22 16:16:10 -0800

    [RISCV] Fix double counting scalar CSRs with Zcmp when emitting cfi_offset for RVV CSRs.
    getCalleeSavedStackSize() already contains RVPushStackSize. Don't
    subtract it again.

--- llvm/lib/Target/RISCV/RISCVFrameLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVFrameLowering.cpp
@@ -1752,8 +1752,7 @@ void RISCVFrameLowering::emitCalleeSavedRVVPrologCFI(
   if (!HasFP) {
     uint64_t ScalarLocalVarSize =
         MFI.getStackSize() - RVFI->getCalleeSavedStackSize() -
-        RVFI->getRVPushStackSize() - RVFI->getVarArgsSaveSize() +
-        RVFI->getRVVPadding();
+        RVFI->getVarArgsSaveSize() + RVFI->getRVVPadding();
     FixedSize -= ScalarLocalVarSize;
   }
 
