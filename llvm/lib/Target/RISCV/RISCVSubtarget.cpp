MIPS/llvm

[RISCV] Add RemoveBackToBackBranches Pass
According to the MIPS specification, there shouldn't be
two conditional branches in the same 8-byte aligned
region of code.

llvm/lib/Target/RISCV/RISCVSubtarget.cpp
@@ -71,6 +71,11 @@ static cl::opt<bool> UseCCMovInsn(
     cl::desc("RISCV: Use 'ccmov' instruction"),
     cl::init(true), cl::Hidden);
 
+static cl::opt<bool> RISCVRemoveBackToBackBranches(
+    "riscv-remove-back-to-back-branches",
+    cl::desc("RISCV: Insert nops to clear pipeline hazards."),
+    cl::init(false), cl::Hidden);
+
 void RISCVSubtarget::anchor() {}
 
 RISCVSubtarget &
@@ -225,4 +230,8 @@ bool RISCVSubtarget::useLoadStorePairs() const {
 
 bool RISCVSubtarget::useCCMovInsn() const {
   return UseCCMovInsn && HasCCMov;
+}
+
+bool RISCVSubtarget::shouldRemoveBackToBackBranches() const {
+  return RISCVRemoveBackToBackBranches && hasFeature(RISCV::Procp8700);
 }

llvm-git-migration/llvm-project

llvm/lib/Target/RISCV/RISCVSubtarget.cpp
@@ -72,6 +72,11 @@ static cl::opt<bool> UseCCMovInsn(
     cl::desc("RISCV: Use 'ccmov' instruction"),
     cl::init(true), cl::Hidden);
 
+static cl::opt<bool> RISCVRemoveBackToBackBranches(
+    "riscv-remove-back-to-back-branches",
+    cl::desc("RISCV: Insert nops to clear pipeline hazards."),
+    cl::init(false), cl::Hidden);
+
 void RISCVSubtarget::anchor() {}
 
 RISCVSubtarget &
@@ -238,4 +243,8 @@ bool RISCVSubtarget::useLoadStorePairs() const {
 
 bool RISCVSubtarget::useCCMovInsn() const {
   return UseCCMovInsn && HasMIPSCMov;
+}
+
+bool RISCVSubtarget::shouldRemoveBackToBackBranches() const {
+  return RISCVRemoveBackToBackBranches && hasFeature(RISCV::TuneMIPSP8700);
 }