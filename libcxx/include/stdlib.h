9d78f8338e106b1572bae9c2237d9b2c742ac179
Louis Dionne <ldionne.2@gmail.com>
2024-12-06 15:20:55 -0500

    [libc++] Remove explicit mentions of __need_FOO macros
    This change has a long history. It was first attempted naively in
    https://reviews.llvm.org/D131425, which didn't work because we broke
    the ability for code to include e.g. <stdio.h> multiple times and get
    different definitions based on the pre-defined macros.
    
    However, in #86843 we managed to simplify <stddef.h> by including
    the underlying system header outside of any include guards, which
    worked.
    
    This patch applies the same simplification we did to <stddef.h> to
    the other headers that currently mention __need_FOO macros explicitly.

--- libcxx/include/stdlib.h
+++ libcxx/include/stdlib.h
@@ -7,17 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if defined(__need_malloc_and_calloc)
-
-#  if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
-#    pragma GCC system_header
-#  endif
-
-#  include_next <stdlib.h>
-
-#elif !defined(_LIBCPP_STDLIB_H)
-#  define _LIBCPP_STDLIB_H
-
 /*
     stdlib.h synopsis
 
@@ -84,11 +73,21 @@ void *aligned_alloc(size_t alignment, size_t size);                       // C11
 
 */
 
-#  include <__config>
+#include <__config>
 
-#  if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
-#    pragma GCC system_header
-#  endif
+#if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
+#  pragma GCC system_header
+#endif
+
+// The inclusion of the system's <stdlib.h> is intentionally done once outside of any include
+// guards because some code expects to be able to include the underlying system header multiple
+// times to get different definitions based on the macros that are set before inclusion.
+#if __has_include_next(<stdlib.h>)
+#  include_next <stdlib.h>
+#endif
+
+#if !defined(_LIBCPP_STDLIB_H)
+#  define _LIBCPP_STDLIB_H
 
 #  if __has_include_next(<stdlib.h>)
 #    include_next <stdlib.h>
@@ -146,6 +145,6 @@ inline _LIBCPP_HIDE_FROM_ABI lldiv_t div(long long __x, long long __y) _NOEXCEPT
 #      endif
 #    endif // _LIBCPP_MSVCRT
 } // extern "C++"
-#  endif   // __cplusplus
+#  endif // __cplusplus
 
 #endif // _LIBCPP_STDLIB_H
