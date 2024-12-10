acd25c672693140d62cbf04cfed608cecd5e9184
Tristan Ross <tristan.ross@midstall.com>
2024-12-09 18:36:14 -0800

    [libc] Add unistd overlay

--- libc/src/unistd/readlink.h
+++ libc/src/unistd/readlink.h
@@ -9,8 +9,10 @@
 #ifndef LLVM_LIBC_SRC_UNISTD_READLINK_H
 #define LLVM_LIBC_SRC_UNISTD_READLINK_H
 
+#include "hdr/unistd_macros.h"
+#include "include/llvm-libc-types/size_t.h"
+#include "include/llvm-libc-types/ssize_t.h"
 #include "src/__support/macros/config.h"
-#include <unistd.h>
 
 namespace LIBC_NAMESPACE_DECL {
 
