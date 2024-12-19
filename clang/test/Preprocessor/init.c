a9fb7e7b26ef375a2a8e2e920f5fcd696d5cf4fc
Tristan Ross <tristan.ross@midstall.com>
2024-12-19 13:40:50 -0800

    [clang] add __uefi__ test for aarch64 & x86_64

--- clang/test/Preprocessor/init.c
+++ clang/test/Preprocessor/init.c
@@ -2742,3 +2742,11 @@
 // RISCV64-LINUX: #define __unix__ 1
 // RISCV64-LINUX: #define linux 1
 // RISCV64-LINUX: #define unix 1
+//
+// RUN: %clang_cc1 -dM -triple=aarch64-unknown-uefi -E /dev/null | FileCheck -match-full-lines -check-prefix uefi %s
+//
+// AARCH64-UEFI: #define __uefi__ 1
+//
+// RUN: %clang_cc1 -dM -triple=x86_64-unknown-uefi -E /dev/null | FileCheck -match-full-lines -check-prefix uefi %s
+//
+// X86-64-UEFI: #define __uefi__ 1
