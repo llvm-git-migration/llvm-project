390055962ee9237070b9d539fda2743e47cc2e4d
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 13:50:32 +0500

    fix: dependencies for timezone for tests

--- libc/test/src/time/asctime_r_test.cpp
+++ libc/test/src/time/asctime_r_test.cpp
@@ -9,10 +9,8 @@
 #include "src/errno/libc_errno.h"
 #include "src/time/asctime_r.h"
 #include "src/time/time_utils.h"
-#include "src/time/timezone.h"
 #include "test/UnitTest/Test.h"
 #include "test/src/time/TmHelper.h"
-#include <stddef.h>
 
 using LIBC_NAMESPACE::time_utils::TimeConstants;
 
