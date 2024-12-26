9db9530a9da971ebf5ce11ca2530fc84a576f9aa
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 11:09:21 +0500

    format code with clang-format

--- libc/test/src/time/gmtime_r_test.cpp
+++ libc/test/src/time/gmtime_r_test.cpp
@@ -6,10 +6,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "src/time/timezone.h"
 #include "src/time/gmtime_r.h"
 #include "src/time/localtime.h"
 #include "src/time/time_utils.h"
+#include "src/time/timezone.h"
 #include "test/UnitTest/Test.h"
 #include "test/src/time/TmMatcher.h"
 
