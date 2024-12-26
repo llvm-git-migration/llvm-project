4886fbef7a07d249a0d10e83fe21279f26cf0dba
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 11:08:09 +0500

    add timezone to gmtime tests

--- libc/test/src/time/gmtime_r_test.cpp
+++ libc/test/src/time/gmtime_r_test.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "src/time/timezone.h"
 #include "src/time/gmtime_r.h"
 #include "src/time/localtime.h"
 #include "src/time/time_utils.h"
