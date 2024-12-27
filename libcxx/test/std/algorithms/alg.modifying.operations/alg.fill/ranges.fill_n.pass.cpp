5a0d168b8cefb2cfef83c98743c30d5f52662dfe
Peng Liu <winner245@hotmail.com>
2024-12-27 08:20:39 -0500

    Augment ranges::{fill, fill_n, find} with missing tests

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.fill/ranges.fill_n.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.fill/ranges.fill_n.pass.cpp
@@ -18,9 +18,11 @@
 #include <cassert>
 #include <ranges>
 #include <string>
+#include <vector>
 
 #include "almost_satisfies_types.h"
 #include "test_iterators.h"
+#include "test_macros.h"
 
 template <class Iter>
 concept HasFillN = requires(Iter iter) { std::ranges::fill_n(iter, int{}, int{}); };
@@ -68,7 +70,7 @@ constexpr bool test() {
     };
 
     S a[5];
-    std::ranges::fill_n(a, 5, S {});
+    std::ranges::fill_n(a, 5, S{});
     assert(std::all_of(a, a + 5, [](S& s) { return s.copied; }));
   }
 
@@ -78,6 +80,20 @@ constexpr bool test() {
     assert(ret == a.data() + a.size());
     assert(std::all_of(a.begin(), a.end(), [](auto& s) { return s == "long long string so no SSO"; }));
   }
+
+#if TEST_STD_VER >= 23
+  { // Test vector<bool>::iterator optimization
+    for (std::size_t N = 8; N <= 256; N *= 2) {
+      // Test with both full and partial bytes
+      for (std::size_t offset : {0, 8}) {
+        std::vector<bool> in(N + offset);
+        std::vector<bool> expected(N + offset, true);
+        std::ranges::fill_n(std::ranges::begin(in) + offset / 2, N + offset / 2, true);
+        assert(std::equal(in.begin() + offset / 2, in.end() - offset / 2, expected.begin() + offset / 2));
+      }
+    }
+  }
+#endif
 
   return true;
 }
