1f01309c237c8daf15be4ac1708e22e841935662
Pranav Kant <prka@google.com>
2024-12-30 14:59:04 +0000

    comment

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -245,7 +245,7 @@ public:
     ///   construction vtable because it hasn't added 'type' as a substitution.
     ///   - Skip mangling enclosing class templates of member-like friend
     ///   function templates.
-    ///   - Incorrectly return C++ records in AVX registers.
+    ///   - Incorrectly return C++ records in AVX registers on x86_64.
     Ver19,
 
     /// Conform to the underlying platform's C and C++ ABIs as closely
