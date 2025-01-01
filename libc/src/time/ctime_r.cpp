ea3d0b93df3396782b4cb016789dbeb71c728ffc
Зишан Мирза <zmirza@posteo.de>
2025-01-01 06:26:26 +0500

    format code with clang-format

--- libc/src/time/ctime_r.cpp
+++ libc/src/time/ctime_r.cpp
@@ -21,8 +21,8 @@ LLVM_LIBC_FUNCTION(char *, ctime_r, (const time_t *t_ptr, char *buffer)) {
     return nullptr;
   }
 
-  return time_utils::asctime(time_utils::localtime_internal(t_ptr, &tm_out), buffer,
-                             TimeConstants::ASCTIME_MAX_BYTES);
+  return time_utils::asctime(time_utils::localtime_internal(t_ptr, &tm_out),
+                             buffer, TimeConstants::ASCTIME_MAX_BYTES);
 }
 
 } // namespace LIBC_NAMESPACE_DECL
