f35c48f75e0ebdbe6dfb797fc9c1ca14d1f6dfcf
Parth Arora <quic_partaror@quicinc.com>
2025-01-02 09:32:23 -0800

    [LLD] [ELF] Add support for linker script unary plus operator
    This commit adds support for linker script unary plus ('+') operator.
    It is helpful for improving compatibility between LLD and GNU LD.
    
    Fixes #118047

--- lld/ELF/ScriptParser.cpp
+++ lld/ELF/ScriptParser.cpp
@@ -1489,6 +1489,9 @@ Expr ScriptParser::readPrimary() {
     Expr e = readPrimary();
     return [=] { return -e().getValue(); };
   }
+  if (consume("+")) {
+    return readPrimary();
+  }
 
   StringRef tok = next();
   std::string location = getCurrentLocation();
