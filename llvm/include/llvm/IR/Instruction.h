6b2ba3f6efc7a6cf1281f98215ed4e42a82e2fbf
Florian Hahn <flo@fhahn.com>
2025-01-15 21:44:46 +0000

    [IR] Provide array with poison-generating metadata IDs.

--- llvm/include/llvm/IR/Instruction.h
+++ llvm/include/llvm/IR/Instruction.h
@@ -503,6 +503,9 @@ public:
   /// Determine whether the the nneg flag is set.
   bool hasNonNeg() const LLVM_READONLY;
 
+  constexpr static const unsigned PoisonGeneratingMetadataIDs[] = {
+      LLVMContext::MD_range, LLVMContext::MD_nonnull, LLVMContext::MD_align};
+
   /// Return true if this operator has flags which may cause this instruction
   /// to evaluate to poison despite having non-poison inputs.
   bool hasPoisonGeneratingFlags() const LLVM_READONLY;
