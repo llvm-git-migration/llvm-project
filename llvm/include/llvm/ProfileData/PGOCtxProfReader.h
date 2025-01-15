39ee074d77b9732cbbe53a1b07ca267ecfeb3966
Mircea Trofin <mtrofin@google.com>
2025-01-15 11:07:42 -0800

    [ctxprof] use yaml for serialization (for testing)

--- llvm/include/llvm/ProfileData/PGOCtxProfReader.h
+++ llvm/include/llvm/ProfileData/PGOCtxProfReader.h
@@ -183,5 +183,7 @@ public:
 
   Expected<std::map<GlobalValue::GUID, PGOCtxProfContext>> loadContexts();
 };
+
+void convertCtxProfToYaml(raw_ostream &OS, const PGOCtxProfContext::CallTargetMapTy &);
 } // namespace llvm
 #endif
