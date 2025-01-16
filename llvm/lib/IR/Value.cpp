2eaae5d728f74184c948d9cba5baec324ad68011
Florian Hahn <flo@fhahn.com>
2025-01-16 12:53:11 +0000

    [Loads] Respect UseDerefAtPointSemantics in isDerefAndAlignedPointer.
    If a pointer gets freed, it may not be dereferenceable any longer, even
    though there is a dominating dereferenceable assumption. As first step,
    only consider assumptions if the function doesn't free memory if
    UseDerefAtPointSemantics is used.

--- llvm/lib/IR/Value.cpp
+++ llvm/lib/IR/Value.cpp
@@ -36,7 +36,7 @@
 
 using namespace llvm;
 
-static cl::opt<unsigned> UseDerefAtPointSemantics(
+cl::opt<unsigned> UseDerefAtPointSemantics(
     "use-dereferenceable-at-point-semantics", cl::Hidden, cl::init(false),
     cl::desc("Deref attributes and metadata infer facts at definition only"));
 
