f9c871b12f7e22f8f24ad89a26ee383e17dfad2a
Joseph Huber <huberjn@outlook.com>
2025-01-20 14:41:34 -0600

    [OpenMP] Port the OpenMP device runtime to direct C++ compilation
    Summary:
    This removes the use of OpenMP offloading to build the device runtime.
    The main benefit here is that we no longer need to rely on offloading
    semantics to build a device only runtime. Things like variants are now
    no longer needed and can just be simple if-defs. In the future, I will
    remove most of the special handling here and fold it into calls to the
    `<gpuintrin.h>` functions instead. Additionally I will rework the
    compilation to make this a separate runtime.
    
    The current plan is to have this, but make including OpenMP and
    offloading either automatically add it, or print a warning if it's
    missing. This will allow us to use a normal CMake workflow and delete
    all the weird 'lets pull the clang binary out of the build' business.
    ```
    -DRUNTIMES_amdgcn-amd-amdhsa_LLVM_ENABLE_RUNTIMES=offload
    -DLLVM_RUNTIME_TARGETS=amdgcn-amd-amdhsa
    ```
    
    After that, linking the OpenMP device runtime will be `-Xoffload-linker
    -lomp`. I.e. no more fat binary business.

--- offload/DeviceRTL/include/Synchronization.h
+++ offload/DeviceRTL/include/Synchronization.h
@@ -15,8 +15,6 @@
 #include "DeviceTypes.h"
 #include "DeviceUtils.h"
 
-#pragma omp begin declare target device_type(nohost)
-
 namespace ompx {
 namespace atomic {
 
@@ -218,7 +216,5 @@ void system(atomic::OrderingTy Ordering);
 } // namespace fence
 
 } // namespace ompx
-
-#pragma omp end declare target
 
 #endif
