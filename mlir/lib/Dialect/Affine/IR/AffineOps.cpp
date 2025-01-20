50fcac3e325d7e19094309481161d7a8a3b3bce4
linuxlonelyeagle <2020382038@qq.com>
2025-01-20 10:38:35 +0800

    if the result of a Pure operation that whose operands are dimensional identifiers,then their results are dimensional identifiers.

--- mlir/lib/Dialect/Affine/IR/AffineOps.cpp
+++ mlir/lib/Dialect/Affine/IR/AffineOps.cpp
@@ -274,7 +274,8 @@ Region *mlir::affine::getAffineScope(Operation *op) {
 // conditions:
 // *) It is valid as a symbol.
 // *) It is an induction variable.
-// *) It is the result of affine apply operation with dimension id arguments.
+// *) It is the result of a `Pure` operation whose operands are valid
+//    dimensional identifiers.
 bool mlir::affine::isValidDim(Value value) {
   // The value must be an index type.
   if (!value.getType().isIndex())
@@ -304,8 +305,8 @@ bool mlir::affine::isValidDim(Value value, Region *region) {
   if (isValidSymbol(value, region))
     return true;
 
-  auto *op = value.getDefiningOp();
-  if (!op) {
+  auto *defOp = value.getDefiningOp();
+  if (!defOp) {
     // This value has to be a block argument for an affine.for or an
     // affine.parallel.
     auto *parentOp = llvm::cast<BlockArgument>(value).getOwner()->getParentOp();
@@ -313,11 +314,15 @@ bool mlir::affine::isValidDim(Value value, Region *region) {
   }
 
   // Affine apply operation is ok if all of its operands are ok.
-  if (auto applyOp = dyn_cast<AffineApplyOp>(op))
-    return applyOp.isValidDim(region);
+  if (isPure(defOp) && llvm::all_of(defOp->getOperands(), [&](Value operand) {
+        return affine::isValidDim(operand, region);
+      })) {
+    return true;
+  }
+
   // The dim op is okay if its operand memref/tensor is defined at the top
   // level.
-  if (auto dimOp = dyn_cast<ShapedDimOpInterface>(op))
+  if (auto dimOp = dyn_cast<ShapedDimOpInterface>(defOp))
     return isTopLevelValue(dimOp.getShapedValue());
   return false;
 }
