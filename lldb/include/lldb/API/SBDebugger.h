f16aab0a3b715cd294d61a4bb7278ff24610598a
Jacob Lalonde <jalalonde@fb.com>
2025-01-21 13:40:23 -0800

    Wire up DAP to listen to external progress events, and list the enumeration in SBDebugger

--- lldb/include/lldb/API/SBDebugger.h
+++ lldb/include/lldb/API/SBDebugger.h
@@ -46,8 +46,8 @@ public:
       eBroadcastBitProgress = lldb::DebuggerBroadcastBit::eBroadcastBitProgress,
       eBroadcastBitWarning = lldb::DebuggerBroadcastBit::eBroadcastBitWarning,
       eBroadcastBitError = lldb::DebuggerBroadcastBit::eBroadcastBitError,
-      eBroadcastBitProgressCategory =
-          lldb::DebuggerBroadcastBit::eBroadcastBitProgressCategory,
+      eBroadcastBitExternalProgress =
+          lldb::DebuggerBroadcastBit::eBroadcastBitExternalProgress,
   };
   SBDebugger();
 
