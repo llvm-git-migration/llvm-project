8eb4af9f6257eae171d465109dc62ff7f6cd9e41
Guojin He <he.guojin@gmail.com>
2025-01-28 14:06:22 -0500

    [MLIR] should use dso_local

--- mlir/lib/Target/LLVMIR/ModuleImport.cpp
+++ mlir/lib/Target/LLVMIR/ModuleImport.cpp
@@ -2106,7 +2106,7 @@ LogicalResult ModuleImport::processFunction(llvm::Function *func) {
       iface.isConvertibleIntrinsic(func->getIntrinsicID()))
     return success();
 
-  bool dsoLocal = func->hasLocalLinkage();
+  bool dsoLocal = func->isDSOLocal();
   CConv cconv = convertCConvFromLLVM(func->getCallingConv());
 
   // Insert the function at the end of the module.
