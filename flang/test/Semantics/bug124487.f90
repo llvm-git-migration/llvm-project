85cbc0550cc8bde867ce7f2d75ab0311c893d85f
Peter Klausler <pklausler@nvidia.com>
2025-01-27 16:47:27 -0800

    [flang] Fix crash on SMP with dummy procedure
    When a separate module procedure is defined with MODULE PROCEDURE,
    the compiler crashes if there is a dummy procedure in the interface
    defined with only a result type.  This is due to the type already
    having been defined on the ProcEntityDetails symbol as part of earlier
    wholesale symbol duplication.  Adjust the code to not define the
    result type of the ProcEntityDetails if it is already present,
    but to verify that it is the same type instead.
    
    Fixes https://github.com/llvm/llvm-project/issues/124487.

---
+++ flang/test/Semantics/bug124487.f90
@@ -0,0 +1,13 @@
+!RUN: %flang_fc1 -fsyntax-only %s 2>&1 | FileCheck --allow-empty %s
+module m
+  interface
+    module subroutine smp(x)
+      character, external :: x
+    end
+  end interface
+end
+submodule (m) sm
+ contains
+  module procedure smp ! crashes here
+  end
+end
