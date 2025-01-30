b5fbc4c7cb7a46ac5deacb1cc354063c41cf23c5
Jonathan Thackray <jonathan.thackray@arm.com>
2025-01-30 11:57:22 +0000

    [AArch64] Add support for 16/32/64-bit floating-point atomic read-modify-write ops
    Add support for 16/32/64-bit floating-point atomic read-modify-write
    operations (FADD, FMAX, FMIN) using LDFADD, LDFMAX, LDFMIN atomic
    instructions.

--- llvm/test/CodeGen/AArch64/Atomics/generate-tests.py
+++ llvm/test/CodeGen/AArch64/Atomics/generate-tests.py
@@ -29,6 +29,20 @@ class Type(enum.Enum):
         return self.name
 
 
+# FP Type name size
+class FPType(enum.Enum):
+    # Value is the size in bytes
+    half = 2
+    float = 4
+    double = 8
+
+    def align(self, aligned: bool) -> int:
+        return self.value if aligned else 1
+
+    def __str__(self) -> str:
+        return self.name
+
+
 # Is this an aligned or unaligned access?
 class Aligned(enum.Enum):
     aligned = True
@@ -111,6 +125,7 @@ class Feature(enum.Flag):
     v8_1a = enum.auto()  # -mattr=+v8.1a, mandatory FEAT_LOR, FEAT_LSE
     rcpc = enum.auto()  # FEAT_LRCPC
     lse2 = enum.auto()  # FEAT_LSE2
+    lsfe = enum.auto()  # FEAT_LSFE
     outline_atomics = enum.auto()  # -moutline-atomics
     rcpc3 = enum.auto()  # FEAT_LSE2 + FEAT_LRCPC3
     lse2_lse128 = enum.auto()  # FEAT_LSE2 + FEAT_LSE128
@@ -125,6 +140,8 @@ class Feature(enum.Flag):
             return "+lse2,+rcpc3"
         if self == Feature.lse2_lse128:
             return "+lse2,+lse128"
+        if self == Feature.lsfe:
+            return "+lsfe"
         return "+" + self.name
 
 
@@ -142,11 +159,17 @@ ATOMICRMW_OPS = [
     "umin",
 ]
 
+FP_ATOMICRMW_OPS = [
+    "fadd",
+    "fsub",
+    "fmax",
+    "fmin",
+]
 
-def all_atomicrmw(f):
-    for op in ATOMICRMW_OPS:
+def all_atomicrmw(f, datatype, atomicrmw_ops):
+    for op in atomicrmw_ops:
         for aligned in Aligned:
-            for ty in Type:
+            for ty in datatype:
                 for ordering in ATOMICRMW_ORDERS:
                     name = f"atomicrmw_{op}_{ty}_{aligned}_{ordering}"
                     instr = "atomicrmw"
@@ -286,7 +309,14 @@ def write_lit_tests():
             with open(f"{triple}-atomicrmw-{feat.name}.ll", "w") as f:
                 filter_args = r'--filter-out "\b(sp)\b" --filter "^\s*(ld[^r]|st[^r]|swp|cas|bl|add|and|eor|orn|orr|sub|mvn|sxt|cmp|ccmp|csel|dmb)"'
                 header(f, triple, [feat], filter_args)
-                all_atomicrmw(f)
+                if (feat != Feature.lsfe):
+                   all_atomicrmw(f, Type, ATOMICRMW_OPS)
+                else:
+                   all_atomicrmw(f, FPType, FP_ATOMICRMW_OPS)
+
+            # Floating point atomics only supported for atomicrmw currently
+            if (feat == Feature.lsfe):
+                continue;
 
             with open(f"{triple}-cmpxchg-{feat.name}.ll", "w") as f:
                 filter_args = r'--filter-out "\b(sp)\b" --filter "^\s*(ld[^r]|st[^r]|swp|cas|bl|add|and|eor|orn|orr|sub|mvn|sxt|cmp|ccmp|csel|dmb)"'
