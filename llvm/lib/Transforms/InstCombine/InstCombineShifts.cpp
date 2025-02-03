9ad122ffdc5c41654a12cc72e160cb237d066505
John Lu <John.Lu@amd.com>
2025-02-03 10:03:27 -0600

    Apply clang-format
    Signed-off-by: John Lu <John.Lu@amd.com>

--- llvm/lib/Transforms/InstCombine/InstCombineShifts.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineShifts.cpp
@@ -1033,8 +1033,8 @@ static bool setShiftFlags(BinaryOperator &I, const SimplifyQuery &Q) {
 }
 
 static Instruction *transformClampedShift64(BinaryOperator &I,
-					    const SimplifyQuery &Q,
-					    InstCombiner::BuilderTy &Builder) {
+                                            const SimplifyQuery &Q,
+                                            InstCombiner::BuilderTy &Builder) {
   Value *Op0 = I.getOperand(0), *Op1 = I.getOperand(1);
   Type *I32Type = Type::getInt32Ty(I.getContext());
   Type *I64Type = Type::getInt64Ty(I.getContext());
@@ -1042,15 +1042,16 @@ static Instruction *transformClampedShift64(BinaryOperator &I,
   if (I.getType() == I64Type) {
     KnownBits KnownAmt = computeKnownBits(Op1, /* Depth */ 0, Q);
     if (KnownAmt.getMinValue().uge(32)) {
-      Value *TruncVal         = Builder.CreateTrunc(Op0, I32Type);
-      Value *TruncShiftAmt    = Builder.CreateTrunc(Op1, I32Type);
-      Value *AdjustedShiftAmt = Builder.CreateSub  (TruncShiftAmt,
-                                                   ConstantInt::get(I32Type, 32));
-      Value *Shl32   = Builder.CreateShl(TruncVal, AdjustedShiftAmt);
-      Value *VResult = Builder.CreateVectorSplat(2, ConstantInt::get(I32Type, 0));
+      Value *TruncVal = Builder.CreateTrunc(Op0, I32Type);
+      Value *TruncShiftAmt = Builder.CreateTrunc(Op1, I32Type);
+      Value *AdjustedShiftAmt =
+          Builder.CreateSub(TruncShiftAmt, ConstantInt::get(I32Type, 32));
+      Value *Shl32 = Builder.CreateShl(TruncVal, AdjustedShiftAmt);
+      Value *VResult =
+          Builder.CreateVectorSplat(2, ConstantInt::get(I32Type, 0));
 
       VResult = Builder.CreateInsertElement(VResult, Shl32,
-                                           ConstantInt::get(I32Type, 1));
+                                            ConstantInt::get(I32Type, 1));
       return CastInst::Create(Instruction::BitCast, VResult, I64Type);
     }
   }
