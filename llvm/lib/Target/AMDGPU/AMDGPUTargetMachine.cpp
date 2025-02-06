5862e3249531359f66ae911fa42c7a69514a82ac
Scott Linder <Scott.Linder@amd.com>
2025-02-06 00:01:07 +0000

    [AMDGPU] Push amdgpu-preload-kern-arg-prolog after livedebugvalues
    This is effectively a workaround for a bug in livedebugvalues, but seems
    to potentially be a general improvement, as BB sections seems like it
    could ruin the special 256-byte prelude scheme that
    amdgpu-preload-kern-arg-prolog requires anyway. Moving it even later
    doesn't seem to have any material impact, and just adds livedebugvalues
    to the list of things which no longer have to deal with pseudo
    multiple-entry functions.
    
    AMDGPU debug-info isn't supported upstream yet, so the bug being avoided
    isn't testable here. I am posting the patch upstream to avoid an
    unnecessary diff with AMD's fork.

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1147,6 +1147,7 @@ public:
   void addPostRegAlloc() override;
   void addPreSched2() override;
   void addPreEmitPass() override;
+  void addPostBBSections() override;
 };
 
 } // end anonymous namespace
@@ -1686,6 +1687,11 @@ void GCNPassConfig::addPreEmitPass() {
     addPass(&AMDGPUInsertDelayAluID);
 
   addPass(&BranchRelaxationPassID);
+}
+
+void GCNPassConfig::addPostBBSections() {
+  // We run this later to avoid passes like livedebugvalues and BBSections
+  // having to deal with the apparent multi-entry functions we may generate.
   addPass(createAMDGPUPreloadKernArgPrologLegacyPass());
 }
 
